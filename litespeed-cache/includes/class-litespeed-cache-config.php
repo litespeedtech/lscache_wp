126193cc16ade4a39748f26eafdf0601e498508b
Kevin Fwu <kfwu@litespeedtech.com>
2016-07-25 15:13:58 -0400

    [REFACTOR] Handle a couple more edge cases

--- litespeed-cache/includes/class-litespeed-cache-config.php
+++ litespeed-cache/includes/class-litespeed-cache-config.php
@@ -277,7 +277,10 @@ class LiteSpeed_Cache_Config
 		}
 		$this->options[self::OPID_VERSION] = LiteSpeed_Cache::PLUGIN_VERSION;
 
-		LiteSpeed_Cache_Admin_Rules::get_instance()->scan_upgrade();
+		if ((!is_multisite()) || (is_network_admin())) {
+			$this->options[self::OPID_LOGIN_COOKIE]
+				= LiteSpeed_Cache_Admin_Rules::get_instance()->scan_upgrade();
+		}
 
 		$res = update_option(self::OPTION_NAME, $this->options) ;
 		$this->debug_log("plugin_upgrade option changed = $res $log\n",
