b2d177595fe8bc2fcdaba8712a985a129e536fd0
Kevin Fwu <kfwu@litespeedtech.com>
2016-08-16 11:10:28 -0400

    [NEW] Add WooCommerce config options.

--- litespeed-cache/thirdparty/class-litespeed-cache-thirdparty-woocommerce.php
+++ litespeed-cache/thirdparty/class-litespeed-cache-thirdparty-woocommerce.php
@@ -13,6 +13,11 @@ class LiteSpeed_Cache_ThirdParty_WooCommerce
 
 	const CACHETAG_SHOP = 'WC_S';
 	const CACHETAG_TERM = 'WC_T.';
+	const OPTION_UPDATE_INTERVAL = 'wc_update_interval';
+	const OPT_PQS_CS = 0; // flush product on quantity + stock change, categories on stock change
+	CONST OPT_PS_CS = 1; // flush product and categories on stock change
+	CONST OPT_PS_CN = 2; // flush product on stock change, categories no flush
+	CONST OPT_PQS_CQS = 3; // flush product and categories on quantity + stock change
 
 	/**
 	 * Detects if WooCommerce is installed.
@@ -27,12 +32,18 @@ class LiteSpeed_Cache_ThirdParty_WooCommerce
 
 			add_action('woocommerce_after_checkout_validation',
 				'LiteSpeed_Cache_ThirdParty_WooCommerce::add_purge');
+			add_filter('litespeed_cache_get_options',
+				'LiteSpeed_Cache_ThirdParty_WooCommerce::get_config');
 
 			if (is_admin()) {
 				add_action('litespeed_cache_on_purge_post',
 					'LiteSpeed_Cache_ThirdParty_WooCommerce::backend_purge');
 				add_action('delete_term_relationships',
 					'LiteSpeed_Cache_ThirdParty_WooCommerce::delete_rel', 10, 2);
+				add_filter('litespeed_cache_add_config_tab',
+					'LiteSpeed_Cache_ThirdParty_WooCommerce::add_config', 10, 3);
+				add_filter('litespeed_cache_save_options',
+					'LiteSpeed_Cache_ThirdParty_WooCommerce::save_config', 10, 2);
 			}
 		}
 	}
@@ -178,7 +189,18 @@ class LiteSpeed_Cache_ThirdParty_WooCommerce
 	 */
 	public static function purge_product($product)
 	{
-		// TODO: add configs.
+		$config = LiteSpeed_Cache::config(self::OPTION_UPDATE_INTERVAL);
+
+		if ($config === self::OPT_PQS_CQS) {
+			self::backend_purge($product->get_id());
+		}
+		elseif (($config !== self::OPT_PQS_CS) && ($product->is_in_stock())) {
+			return;
+		}
+		elseif (($config !== self::OPT_PS_CN) && (!$product->is_in_stock())) {
+			self::backend_purge($product->get_id());
+		}
+
 		LiteSpeed_Cache_Tags::add_purge_tag(
 			LiteSpeed_Cache_Tags::TYPE_POST . $product->get_id());
 	}
@@ -229,6 +251,115 @@ class LiteSpeed_Cache_ThirdParty_WooCommerce
 				LiteSpeed_Cache_Tags::add_purge_tag(self::CACHETAG_TERM . $tag);
 			}
 		}
+	}
+
+	/**
+	 * Hooked to the litespeed_cache_get_options filter.
+	 * This will return the option names needed as well as the default options.
+	 *
+	 * @param array $configs
+	 * @return type
+	 */
+	public static function get_config($configs)
+	{
+		if (!is_array($configs)) {
+			return $configs;
+		}
+		$configs[] = self::OPTION_UPDATE_INTERVAL;
+		return $configs;
+	}
+
+	/**
+	 * Hooked to the litespeed_cache_add_config_tab filter.
+	 * Adds the integration configuration options (currently, to determine
+	 * purge rules)
+	 *
+	 * @param array $tabs Third party tabs added.
+	 * @param array $options Current options used.
+	 * @param string $option_group The option group to surround the option id.
+	 * @return mixed False on failure, updated tabs otherwise.
+	 */
+	public static function add_config($tabs, $options, $option_group)
+	{
+		$title = __('WooCommerce Configurations', 'litespeed-cache');
+		$slug = 'woocom';
+		$selected_value = 0;
+		$seloptions = array(
+			__('Purge product on changes to the quantity or stock status.', 'litespeed-cache')
+			. __(' Purge categories only when stock status changes.', 'litespeed-cache'),
+			__('Purge product and categories only when the stock status changes.', 'litespeed-cache'),
+			__('Purge product only when the stock status changes.', 'litespeed-cache')
+			. __(' Do not purge categories on changes to the quantity or stock status.', 'litespeed-cache'),
+			__('Always purge both product and categories on changes to the quantity or stock status.', 'litespeed-cache'),
+		);
+		$desc =
+			__('Determines how changes in product quantity and product stock status affect product pages and their associated category pages.', 'litespeed-cache');
+
+		if ($tabs === false) {
+			return $tabs;
+		}
+
+		if (isset($options) && isset($options[self::OPTION_UPDATE_INTERVAL])) {
+			$selected_value = $options[self::OPTION_UPDATE_INTERVAL];
+		}
+
+		$content = '<hr/><h3 class="title">'
+			. $title . "</h3>\n"
+			. '<table class="form-table">' . "\n"
+			. '<tr><th scope="row">'
+			. __('Product Update Interval', 'litespeed-cache') . '</th><td>';
+
+		$content .= '<select name="' . $option_group . '['
+			. self::OPTION_UPDATE_INTERVAL . ']" id="'
+			. self::OPTION_UPDATE_INTERVAL . '">';
+		foreach ( $seloptions as $val => $label ) {
+			$content .= '<option value="' . $val . '"' ;
+			if ( $selected_value == $val ) {
+				$content .= ' selected="selected"' ;
+			}
+			$content .= '>' . $label . '</option>' ;
+		}
+		$content .= '</select><p class="description">' . $desc
+			. "</p></td></tr>\n</table>\n";
+
+		$tab = array(
+			'title' => $title,
+			'slug' => $slug,
+			'content' => $content
+		);
+
+		$tabs[] = $tab;
+
+		return $tabs;
+	}
+
+	/**
+	 * Hooked to the litespeed_cache_save_options filter.
+	 * Parses the input for this integration's options and updates
+	 * the options array accordingly.
+	 *
+	 * @param array $options The saved options array.
+	 * @param array $input The input options array.
+	 * @return mixed false on failure, updated $options otherwise.
+	 */
+	public static function save_config($options, $input)
+	{
+		if (!isset($options)) {
+			return $options;
+		}
+		$update_val_in = $input[self::OPTION_UPDATE_INTERVAL];
+		switch ($update_val_in) {
+			case self::OPT_PQS_CS:
+			case self::OPT_PS_CS:
+			case self::OPT_PS_CN:
+			case self::OPT_PQS_CQS:
+				$options[self::OPTION_UPDATE_INTERVAL] = intval($update_val_in);
+				break;
+			default:
+				// add error message?
+				break;
+		}
+		return $options;
 	}
 
 }
