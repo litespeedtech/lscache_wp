993888076455e0cf74efcbb2d4328f490a2a838b
Hai Zheng <hzheng@litespeedtech.com>
2017-06-20 15:10:46 -0400

    [VERSION] v1.1.2 version number

--- litespeed-cache/includes/class-litespeed-cache.php
+++ litespeed-cache/includes/class-litespeed-cache.php
@@ -19,7 +19,7 @@ class LiteSpeed_Cache
 	private $config;
 
 	const PLUGIN_NAME = 'litespeed-cache';
-	const PLUGIN_VERSION = '1.1.1.1';
+	const PLUGIN_VERSION = '1.1.2';
 
 	const LSCOOKIE_VARY_NAME = 'LSCACHE_VARY_COOKIE' ;
 	const LSCOOKIE_DEFAULT_VARY = '_lscache_vary' ;
