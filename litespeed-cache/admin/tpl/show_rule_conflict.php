02980df9e8fafb88d0a63daed13fd4fd53526e5f
Hai Zheng <hzheng@litespeedtech.com>
2017-08-07 14:47:43 -0400

    [UPDATE] removed the ExpiresDefault rule conflict check

--- litespeed-cache/admin/tpl/show_rule_conflict.php
+++ litespeed-cache/admin/tpl/show_rule_conflict.php
@@ -2,7 +2,7 @@
 if ( ! defined( 'WPINC' ) ) die ;
 
 
-$err = sprintf( __( 'In %s file found one unexpected cache rule %s left by other cache plugins. Please remove it to get LiteSpeed Cache Plugin work well. If you are sure it is left by purpose, please dismiss this message. ', 'litespeed-cache' ), '.htaccess', '`ExpiresDefault`' );
+$err = sprintf( __( 'Unexpected cache rule %2$s found in %1$s file. This rule may cause visitors to see old versions of pages due to the browser caching html pages. If you are sure that html pages are not being browser cached, this message can be dismissed.', 'litespeed-cache' ), '.htaccess', '`ExpiresDefault`' );
 
 // other plugin left cache expired rules in .htaccess which will cause conflicts
 self::add_notice(self::NOTICE_YELLOW . ' lscwp-notice-ruleconflict', $err);
