9b503d12f70689b1162a55b327015b0349db8be6
Hai Zheng <hzheng@litespeedtech.com>
2017-11-06 14:17:06 -0500

    [IMPROVEMENT] removed emoji styles from frontend pages(Thanks to @kaiderella)

--- litespeed-cache/inc/optimize.class.php
+++ litespeed-cache/inc/optimize.class.php
@@ -132,6 +132,13 @@ class LiteSpeed_Cache_Optimize
 		remove_action( 'admin_print_scripts' , 'print_emoji_detection_script' ) ;
 		remove_filter( 'the_content_feed' , 'wp_staticize_emoji' ) ;
 		remove_filter( 'comment_text_rss' , 'wp_staticize_emoji' ) ;
+		/**
+		 * Added for better result
+		 * @since  1.6.3
+		 */
+		remove_action( 'wp_print_styles', 'print_emoji_styles' ) ;
+		remove_action( 'admin_print_styles', 'print_emoji_styles' ) ;
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' ) ;
 	}
 
 	/**
