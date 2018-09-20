5bd0f6ce9c52fc818370e69184a8f495e5146749
Hai <hzheng@litespeedtech.com>
2018-09-20 09:42:07 -0400

    [GUI] moved inline minify to under html minify due to the dependency

--- litespeed-cache/admin/tpl/setting/settings_optimize.php
+++ litespeed-cache/admin/tpl/setting/settings_optimize.php
@@ -35,16 +35,6 @@ $last_critical_css_generated = LiteSpeed_Cache_CSS::get_summary() ;
 	</tr>
 
 	<tr>
-		<th><?php echo __( 'Inline CSS Minify', 'litespeed-cache' ) ; ?></th>
-		<td>
-			<?php $this->build_switch( LiteSpeed_Cache_Config::OPID_CSS_INLINE_MINIFY ) ; ?>
-			<div class="litespeed-desc">
-				<?php echo __( 'Minify inline CSS code.', 'litespeed-cache' ) ; ?>
-			</div>
-		</td>
-	</tr>
-
-	<tr>
 		<th><?php echo __( 'CSS Combine', 'litespeed-cache' ) ; ?></th>
 		<td>
 			<?php $this->build_switch( LiteSpeed_Cache_Config::OPID_CSS_COMBINE ) ; ?>
@@ -71,16 +61,6 @@ $last_critical_css_generated = LiteSpeed_Cache_CSS::get_summary() ;
 			<?php $this->build_switch( LiteSpeed_Cache_Config::OPID_JS_MINIFY ) ; ?>
 			<div class="litespeed-desc">
 				<?php echo __( 'Minify JS files.', 'litespeed-cache' ) ; ?>
-			</div>
-		</td>
-	</tr>
-
-	<tr>
-		<th><?php echo __( 'Inline JS Minify', 'litespeed-cache' ) ; ?></th>
-		<td>
-			<?php $this->build_switch( LiteSpeed_Cache_Config::OPID_JS_INLINE_MINIFY ) ; ?>
-			<div class="litespeed-desc">
-				<?php echo __( 'Minify inline JS code.', 'litespeed-cache' ) ; ?>
 			</div>
 		</td>
 	</tr>
@@ -124,6 +104,26 @@ $last_critical_css_generated = LiteSpeed_Cache_CSS::get_summary() ;
 			<?php $this->build_switch( LiteSpeed_Cache_Config::OPID_HTML_MINIFY ) ; ?>
 			<div class="litespeed-desc">
 				<?php echo __( 'Minify HTML content.', 'litespeed-cache' ) ; ?>
+			</div>
+		</td>
+	</tr>
+
+	<tr>
+		<th class="litespeed-padding-left"><?php echo __( 'Inline CSS Minify', 'litespeed-cache' ) ; ?></th>
+		<td>
+			<?php $this->build_switch( LiteSpeed_Cache_Config::OPID_CSS_INLINE_MINIFY ) ; ?>
+			<div class="litespeed-desc">
+				<?php echo __( 'Minify inline CSS code.', 'litespeed-cache' ) ; ?>
+			</div>
+		</td>
+	</tr>
+
+	<tr>
+		<th class="litespeed-padding-left"><?php echo __( 'Inline JS Minify', 'litespeed-cache' ) ; ?></th>
+		<td>
+			<?php $this->build_switch( LiteSpeed_Cache_Config::OPID_JS_INLINE_MINIFY ) ; ?>
+			<div class="litespeed-desc">
+				<?php echo __( 'Minify inline JS code.', 'litespeed-cache' ) ; ?>
 			</div>
 		</td>
 	</tr>
