e047fb59c94cdd60060bc47cff547276337c1e12
Hai <hzheng@litespeedtech.com>
2019-02-28 07:59:01 -0500

    🔥🐞 Potential warning right after upgrade process when have OC on

--- litespeed-cache/inc/utility.class.php
+++ litespeed-cache/inc/utility.class.php
@@ -18,6 +18,18 @@ class LiteSpeed_Cache_Utility
 	const TYPE_SCORE_CHK = 'score_chk' ;
 
 	/**
+	 * Check if an URL or current page is REST req or not
+	 *
+	 * @since  2.9.3
+	 * @deprecated 2.9.4 Moved to REST class
+	 * @access public
+	 */
+	public static function is_rest( $url = false )
+	{
+		return false ;
+	}
+
+	/**
 	 * Check page score
 	 *
 	 * @since  2.9
