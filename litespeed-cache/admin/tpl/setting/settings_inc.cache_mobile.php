95a6f05deef2b375c04e20c5ad4c8648b197f601
Hai <hzheng@litespeedtech.com>
2019-03-04 09:47:06 -0500

    [DOC] Cache Mobile desc. (@johnnynguyen)

--- litespeed-cache/admin/tpl/setting/settings_inc.cache_mobile.php
+++ litespeed-cache/admin/tpl/setting/settings_inc.cache_mobile.php
@@ -11,8 +11,8 @@ if (!defined('WPINC')) die;
 				$this->build_switch(LiteSpeed_Cache_Config::OPID_CACHE_MOBILE);
 			?>
 			<div class="litespeed-desc">
-				<?php echo __('When enabled, mobile views will be cached separately.', 'litespeed-cache'); ?>
-				<?php echo __('A site built with responsive design does not need to check this.', 'litespeed-cache'); ?>
+				<?php echo __('Cache mobile views separately.', 'litespeed-cache'); ?>
+				<?php echo __('Only enable for AMP or mobile-specific design/content, not for responsive sites.', 'litespeed-cache'); ?>
 				<br /><font class="litespeed-warning">
 					⚠️
 					<?php echo __( 'This setting will edit the .htaccess file.', 'litespeed-cache' ) ; ?>
