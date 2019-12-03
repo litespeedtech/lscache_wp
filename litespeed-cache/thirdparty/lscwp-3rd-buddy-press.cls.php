71540c8df33314d67d16a71586bc2109ad24e7e5
Stanley <fung1020hk@gmail.com>
2019-12-04 00:32:17 +0800

    Add auto purge for profile editing

--- litespeed-cache/thirdparty/lscwp-3rd-buddy-press.cls.php
+++ litespeed-cache/thirdparty/lscwp-3rd-buddy-press.cls.php
@@ -25,8 +25,23 @@ class LiteSpeed_Cache_ThirdParty_Buddy_Press
 		add_filter( 'wp_nav_menu', 'LiteSpeed_Cache_ThirdParty_Buddy_Press::make_esi_menu', 10, 2 ) ;
 		LiteSpeed_Cache_API::hook_tpl_esi( 'bp-menu', 'LiteSpeed_Cache_ThirdParty_Buddy_Press::hook_esi' ) ;
 
-		add_action( 'wp_login', 'LiteSpeed_Cache_ThirdParty_Buddy_Press::buddypress_login_tag', 10, 2 ) ;
-		add_filter( 'xprofile_updated_profile', 'LiteSpeed_Cache_ThirdParty_Buddy_Press::purge_on_profile_update' ) ;
+		/*
+		* Add tag to every users for purge on action
+		*/
+		if ( is_user_logged_in() ) {
+			LiteSpeed_Cache_API::tag_add( 'buddy_press_user_' . get_current_user_id() ) ;
+		}
+
+		// Purge on profile update
+		add_action( 'xprofile_updated_profile', 'LiteSpeed_Cache_ThirdParty_Buddy_Press::purge_on_profile_update' ) ;
+		// Purge on avatar update
+		add_action( 'xprofile_avatar_uploaded', 'LiteSpeed_Cache_ThirdParty_Buddy_Press::purge_on_profile_update' ) ;
+		// Purge on cover image update
+		if ( ! empty( $_POST[ 'action' ] ) && $_POST[ 'action' ] === 'bp_cover_image_upload' ) {
+			if ( empty( $bp_params = $_POST[ 'bp_params' ] ) ) return ;
+			if ( empty( $user_id = $bp_params[ 'item_id' ] ) ) return ;
+			LiteSpeed_Cache_ThirdParty_Buddy_Press::purge_on_profile_update( $user_id ) ;
+		}
 	}
 
 	public static function make_esi_menu( $nav_menu, $args )
@@ -43,19 +58,14 @@ class LiteSpeed_Cache_ThirdParty_Buddy_Press
 		return $nav_menu ;
 	}
 
-	function hook_esi( $args )
+	public static function hook_esi( $args )
 	{
 		$menu_array = array( 'menu' => $args[ 0 ], 'menu_class' => $args[ 1 ] ) ;
 		wp_nav_menu( $menu_array ) ;
 		exit;
 	}
 
-	public static function buddypress_login_tag( $user_login, $user ) {
-		LiteSpeed_Cache_API::add_private( $user->ID ) ;
-	}
-
-	function purge_on_profile_update( $id ) {
-		error_log($id);
-		LiteSpeed_Cache_API::purge_private_all();
+	public static function purge_on_profile_update( $user_id ) {
+		LiteSpeed_Cache_API::purge( 'buddy_press_user_' . $user_id ) ;
 	}
 }
