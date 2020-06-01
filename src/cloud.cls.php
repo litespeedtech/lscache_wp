e41d0af2d689306322c472accfcb3ee58213ef87
Hai <hzheng@litespeedtech.com>
2020-06-01 10:07:30 -0400

    3.2.1-rc2: LQIP/CCSS rate limit improvement. (@ianpegg)

--- src/cloud.cls.php
+++ src/cloud.cls.php
@@ -458,14 +458,20 @@ class Cloud extends Base
 	 */
 	private function _maybe_cloud( $service_tag )
 	{
+		// we don't want the `img_optm-taken` to fail at any given time
+		if ( $service_tag == self::IMGOPTM_TAKEN ) {
+			return true;
+		}
+
 		// Limit frequent unfinished request to 5min
-		if ( ! empty( $this->_summary[ 'last_request.' . $service_tag ] ) ) {
-			$expired = $this->_summary[ 'last_request.' . $service_tag ] + self::EXPIRATION_REQ - time();
+		$timestamp_tag = 'curr_request.';
+		if ( $service_tag == self::SVC_IMG_OPTM . '-' . Img_Optm::TYPE_NEW_REQ ) {
+			$timestamp_tag = 'last_request.';
+		}
+
+		if ( ! empty( $this->_summary[ $timestamp_tag . $service_tag ] ) ) {
+			$expired = $this->_summary[ $timestamp_tag . $service_tag ] + self::EXPIRATION_REQ - time();
 			if ( $expired > 0 ) {
-				if ( $service_tag == self::IMGOPTM_TAKEN) {
-					// we don't want the `img_optm-taken` to fail at any given time
-					return true;
-				}
 				Debug2::debug( "[Cloud] ❌ try [$service_tag] after $expired seconds" );
 
 				if ( $service_tag !== self::API_VER ) {
