1792a4f7a34ce5e0516018febe7ff32af88ec186
Joshua Reynolds <litespeed.j.rey@gmail.com>
2020-06-11 04:30:53 -0500

    Misc minor edits & doc link updates

--- tpl/inc/show_rule_conflict.php
+++ tpl/inc/show_rule_conflict.php
@@ -3,10 +3,10 @@ namespace LiteSpeed ;
 defined( 'WPINC' ) || exit ;
 
 
-$err = sprintf( __( 'Unexpected cache rule %2$s found in %1$s file. This rule may cause visitors to see old versions of pages due to the browser caching html pages. If you are sure that html pages are not being browser cached, this message can be dismissed. (<a %3$s>Learn More</a>)', 'litespeed-cache' ),
+$err = sprintf( __( 'Unexpected cache rule %2$s found in %1$s file. This rule may cause visitors to see old versions of pages due to the browser caching HTML pages. If you are sure that HTML pages are not being browser cached, this message can be dismissed. (<a %3$s>Learn More</a>)', 'litespeed-cache' ),
 		'.htaccess',
 		'`ExpiresDefault`',
-		'href="https://www.litespeedtech.com/support/wiki/doku.php/litespeed_wiki:cache:lscwp:troubleshooting:browser_displays_stale_content" target="_blank"'
+		'href="https://docs.litespeedtech.com/lscache/lscwp/troubleshoot/#browser-displays-stale-content" target="_blank"'
 );
 
 // other plugin left cache expired rules in .htaccess which will cause conflicts
