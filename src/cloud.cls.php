5530019d07a9a245af4d7647dbaba136fdc3d652
Hai <hzheng@litespeedtech.com>
2020-09-03 14:18:10 -0400

    API to dismiss domain key required banner. (#240643)

--- src/cloud.cls.php
+++ src/cloud.cls.php
@@ -682,7 +682,7 @@ class Cloud extends Base {
 	 * @access public
 	 */
 	public function show_promo() {
-		if ( ! $this->_api_key ) {
+		if ( ! $this->_api_key && apply_filters( 'litespeed_dismiss_domain_key', true ) ) {
 			Admin_Display::error( Error::msg( 'lack_of_api_key' ), true );
 		}
 
