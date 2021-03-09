8e3ecf06ce2011cb01d55e84517c8bb5a1655384
Hai Zheng <hzheng@litespeedtech.com>
2021-03-09 12:35:33 -0500

    v3.7-rc19: [Object] OC wont break the plugin install anymore after LSCWP is deactivated by upgrade error

--- lib/object-cache.php
+++ lib/object-cache.php
@@ -1,41 +1,43 @@
 <?php
-defined( 'WPINC' ) || exit ;
+defined( 'WPINC' ) || exit;
 /**
  * LiteSpeed Object Cache
  *
  * @since  1.8
  */
 
-! defined( 'LSCWP_OBJECT_CACHE' ) && define( 'LSCWP_OBJECT_CACHE', true ) ;
+! defined( 'LSCWP_OBJECT_CACHE' ) && define( 'LSCWP_OBJECT_CACHE', true );
 
 // Initialize const `LSCWP_DIR` and locate LSCWP plugin foder
-$lscwp_dir = ( defined( 'WP_PLUGIN_DIR' ) ? WP_PLUGIN_DIR : WP_CONTENT_DIR . '/plugins' ) . '/litespeed-cache/' ;
+$lscwp_dir = ( defined( 'WP_PLUGIN_DIR' ) ? WP_PLUGIN_DIR : WP_CONTENT_DIR . '/plugins' ) . '/litespeed-cache/';
 // Use plugin as higher priority than MU plugin
 if ( ! file_exists( $lscwp_dir . 'litespeed-cache.php' ) ) {
 	// Check if is mu plugin or not
-	$lscwp_dir = ( defined( 'WPMU_PLUGIN_DIR' ) ? WPMU_PLUGIN_DIR : WP_CONTENT_DIR . '/mu-plugins' ) . '/litespeed-cache/' ;
+	$lscwp_dir = ( defined( 'WPMU_PLUGIN_DIR' ) ? WPMU_PLUGIN_DIR : WP_CONTENT_DIR . '/mu-plugins' ) . '/litespeed-cache/';
 	if ( ! file_exists( $lscwp_dir . 'litespeed-cache.php' ) ) {
-		$lscwp_dir = '' ;
+		$lscwp_dir = '';
 	}
 }
 
-$data_file = WP_CONTENT_DIR . '/.object-cache.ini' ;
-$lib_file = $lscwp_dir . 'src/object.lib.php' ;
+$data_file = WP_CONTENT_DIR . '/.object-cache.ini';
+$lib_file = $lscwp_dir . 'src/object.lib.php';
 
 // Can't find LSCWP location, terminate object cache process
 if ( ! $lscwp_dir || ! file_exists( $data_file ) || ( ! file_exists( $lib_file ) ) ) {
 	if ( ! is_admin() ) { // Bypass object cache for frontend
-		require_once ABSPATH . WPINC . '/cache.php' ;
+		require_once ABSPATH . WPINC . '/cache.php';
 	}
 	else {
-		$err = 'Can NOT find LSCWP path for object cache initialization in ' . __FILE__ ;
-		error_log( $err ) ;
-		echo $err ;
+		$err = 'Can NOT find LSCWP path for object cache initialization in ' . __FILE__;
+		error_log( $err );
+		add_action( is_network_admin() ? 'network_admin_notices' : 'admin_notices', function() use ( &$err ) {
+			echo $err;
+		} );
 	}
 }
 else {
 	// Init object cache & LSCWP
 	if ( file_exists( $lib_file ) ) {
-		require_once $lib_file ;
+		require_once $lib_file;
 	}
 }
