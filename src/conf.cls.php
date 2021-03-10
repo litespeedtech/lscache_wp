ebf3d470d17d8121a84c435d0df6a2deaaf9fe8c
Hai Zheng <hzheng@litespeedtech.com>
2021-03-10 11:46:00 -0500

    v3.6.3-rc2: [-] Fixed Network activation warning due to delayed file update in v3.6.3-rc1.

--- src/conf.cls.php
+++ src/conf.cls.php
@@ -115,16 +115,6 @@ class Conf extends Base {
 			}
 		}
 
-		// Activation delayed file update
-		if ( self::get_option( '__activation' ) ) {
-			// Check new version @since 2.9.3
-			Cloud::version_check( 'activate' . ( defined( 'LSCWP_REF' ) ? '_' . LSCWP_REF : '' ) );
-
-			$this->update_confs(); // Files only get corrected in activation or saving settings actions.
-
-			self::delete_option( '__activation' );
-		}
-
 		/**
 		 * Sync latest new options
 		 */
@@ -150,6 +140,15 @@ class Conf extends Base {
 		 */
 		$this->_try_load_site_options();
 
+		// Activation delayed file update
+		if ( self::get_option( '__activation' ) ) {
+			// Check new version @since 2.9.3
+			Cloud::version_check( 'activate' . ( defined( 'LSCWP_REF' ) ? '_' . LSCWP_REF : '' ) );
+
+			$this->update_confs(); // Files only get corrected in activation or saving settings actions.
+
+			self::delete_option( '__activation' );
+		}
 	}
 
 	/**
