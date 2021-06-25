395f32259e9ccf3b5b26374ff5939e49cd7bb890
Hai Zheng <hzheng@litespeedtech.com>
2021-06-25 09:25:39 -0400

    Update doc.cls.php
    Non-translatable string better put as a placeholder.

--- src/doc.cls.php
+++ src/doc.cls.php
@@ -102,7 +102,7 @@ class Doc {
 	 */
 	public static function notice_ips() {
 		echo '<div class="litespeed-primary">';
-		echo '⚠️ ' . __( 'For online services to work correctly, you must whitelist all QUIC.cloud server IPs.', 'litespeed-cache' ) . '<br/>';
+		echo '⚠️ ' . sprintf( __( 'For online services to work correctly, you must whitelist all %s server IPs.', 'litespeed-cache' ), 'QUIC.cloud' ) . '<br/>';
 		echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . __( 'Before generating key, please verify all IPs on this list are whitelisted', 'litespeed-cache' ) . ': ';
 		echo '<a href="' . Cloud::CLOUD_SERVER . '/ips" target="_blank">' . __( 'Current Online Server IPs', 'litespeed-cache' ) . '</a>';
 		echo '</div>';
