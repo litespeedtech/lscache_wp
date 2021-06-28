78c86cc98605eeb5701bebaebade7e6fc5144da9
Joshua Reynolds <litespeed.j.rey@gmail.com>
2021-06-28 14:16:56 -0500

    More UI/UX revisions & CDN page error fix (#367)
    * Revised browser console messages
    
    * Fix regression
    
    * UI revisions

--- assets/js/lazyload.init.js
+++ assets/js/lazyload.init.js
@@ -17,7 +17,7 @@
 	}
 
 	var init = function(){
-		console.log( '[LiteSpeed] Start Lazyload image' )
+		console.log( '[LiteSpeed] Start Lazy Load Images' )
 		instance = new LazyLoad( { elements_selector: "[data-lazyloaded]", callback_finish: litespeed_finish_callback } );
 
 		update_lazyload = function(){
