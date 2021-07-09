83b62a71694c3be9e155d66bf799b9df1e7081b7
Alice Tang <alicetang2cool@gmail.com>
2021-07-09 12:22:55 -0400

    Enhance AMP Compatibility

--- thirdparty/amp.cls.php
+++ thirdparty/amp.cls.php
@@ -13,8 +13,24 @@ defined( 'WPINC' ) || exit ;
 
 use \LiteSpeed\API ;
 
-class AMP
-{
+class AMP {
+	/**
+	 * @since 4.2
+	 */
+	private static function maybe_amp( $amp_function ) {
+		if ( is_admin() ) {
+			return;
+		}
+		if ( ! isset( $_GET[ 'amp' ] ) && ( ! function_exists( $amp_function ) || ! $amp_function() ) ) {
+			return;
+		}
+		! defined( 'LITESPEED_NO_PAGEOPTM' ) && define( 'LITESPEED_NO_PAGEOPTM', true );
+		! defined( 'LITESPEED_NO_LAZY' ) && define( 'LITESPEED_NO_LAZY', true );
+		! defined( 'LITESPEED_GUEST' ) && define( 'LITESPEED_GUEST', false );
+	}
+	public static function maybe_acc_mob_pages() {
+		self::maybe_amp( 'ampforwp_is_amp_endpoint' );
+	}
 	/**
 	 * CSS async will affect AMP result and
 	 * Lazyload will inject JS library which AMP not allowed
@@ -25,10 +41,11 @@ class AMP
 	 */
 	public static function preload()
 	{
-		if ( ! function_exists( 'is_amp_endpoint' ) || is_admin() || ! isset( $_GET[ 'amp' ] ) ) return;
-		! defined( 'LITESPEED_NO_PAGEOPTM' ) && define( 'LITESPEED_NO_PAGEOPTM', true );
-		! defined( 'LITESPEED_NO_LAZY' ) && define( 'LITESPEED_NO_LAZY', true );
-		! defined( 'LITESPEED_GUEST' ) && define( 'LITESPEED_GUEST', false );
+		// ampforwp_is_amp_endpoint() from Accelerated Mobile Pages
+		add_action( 'wp', __CLASS__ . '::maybe_acc_mob_pages' ) ;
+
+		// amp_is_request() from AMP
+		self::maybe_amp( 'amp_is_request' );
 		// add_filter( 'litespeed_can_optm', '__return_false' );
 		// do_action( 'litespeed_conf_force', API::O_OPTM_CSS_ASYNC, false );
 		// do_action( 'litespeed_conf_force', API::O_MEDIA_LAZY, false );
