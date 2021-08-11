118ffbd15db874699d6bef3e9e49f0062b15494a
Hai Zheng <hzheng@litespeedtech.com>
2021-08-11 13:26:27 -0400

    [3rd] Fixed a PHP warning when using Google AMP w/ `/amp` as structure. (@thanhstran98)

--- thirdparty/amp.cls.php
+++ thirdparty/amp.cls.php
@@ -17,13 +17,16 @@ class AMP {
 	/**
 	 * @since 4.2
 	 */
-	private static function maybe_amp( $amp_function ) {
+	private static function _maybe_amp( $amp_function ) {
 		if ( is_admin() ) {
 			return;
 		}
 		if ( ! isset( $_GET[ 'amp' ] ) && ( ! function_exists( $amp_function ) || ! $amp_function() ) ) {
 			return;
 		}
+
+		do_action( 'litespeed_debug', '[3rd] ❌ AMP disabled page optm/lazy' );
+
 		! defined( 'LITESPEED_NO_PAGEOPTM' ) && define( 'LITESPEED_NO_PAGEOPTM', true );
 		! defined( 'LITESPEED_NO_LAZY' ) && define( 'LITESPEED_NO_LAZY', true );
 		! defined( 'LITESPEED_NO_OPTM' ) && define( 'LITESPEED_NO_OPTM', true );
@@ -36,7 +39,16 @@ class AMP {
 	 * @since 4.2
 	 */
 	public static function maybe_acc_mob_pages() {
-		self::maybe_amp( 'ampforwp_is_amp_endpoint' );
+		self::_maybe_amp( 'ampforwp_is_amp_endpoint' );
+	}
+
+	/**
+	 * Google AMP fix
+	 *
+	 * @since 4.2.0.1
+	 */
+	public static function maybe_google_amp() {
+		self::_maybe_amp( 'amp_is_request' );
 	}
 
 	/**
@@ -49,9 +61,10 @@ class AMP {
 	 */
 	public static function preload() {
 		add_action( 'wp', __CLASS__ . '::maybe_acc_mob_pages' );
+		add_action( 'wp', __CLASS__ . '::maybe_google_amp' );
 
 		// amp_is_request() from AMP
-		self::maybe_amp( 'amp_is_request' );
+		// self::maybe_amp( 'amp_is_request' );
 		// add_filter( 'litespeed_can_optm', '__return_false' );
 		// do_action( 'litespeed_conf_force', API::O_OPTM_CSS_ASYNC, false );
 		// do_action( 'litespeed_conf_force', API::O_MEDIA_LAZY, false );
