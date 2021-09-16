b2019b19dabec45b4ed5b6a561c75c07b4f2cc26
Hai Zheng <hzheng@litespeedtech.com>
2021-09-16 11:11:59 -0400

    v4.4.1-a4: * **ESI** Fixed ESI failure on non-cached pages caused by constant `DONOTCACHEPAGE`.

--- src/control.cls.php
+++ src/control.cls.php
@@ -530,11 +530,16 @@ class Control extends Root {
 	 * @return string empty string if empty, otherwise the cache control header.
 	 */
 	public function output() {
+		$esi_hdr = '';
+		if ( ESI::has_esi() ) {
+			$esi_hdr = ',esi=on';
+		}
+
 		$hdr = self::X_HEADER . ': ';
 
 		if ( defined( 'DONOTCACHEPAGE' ) && apply_filters( 'litespeed_const_DONOTCACHEPAGE', DONOTCACHEPAGE ) ) {
 			Debug2::debug( "[Ctrl] ❌ forced no cache [reason] DONOTCACHEPAGE const" );
-			$hdr .= 'no-cache';
+			$hdr .= 'no-cache' . $esi_hdr;
 			return $hdr;
 		}
 
@@ -554,14 +559,12 @@ class Control extends Root {
 				$hdr .= ',max-age=' . $this->get_ttl();
 			}
 
+			$hdr .= $esi_hdr;
+
 			return $hdr;
 		}
 
-		$esi_hdr = '';
 		// Fix cli `uninstall --deactivate` fatal err
-		if ( ESI::has_esi() ) {
-			$esi_hdr = ',esi=on';
-		}
 
 		if ( ! self::is_cacheable() ) {
 			$hdr .= 'no-cache' . $esi_hdr;
