90c999173281f31a2a500e969c0e5c28c4643b8e
Hai Zheng <hzheng@litespeedtech.com>
2021-11-09 14:41:20 -0500

    Only keep REMOTE_ADDR for IP validation.

--- src/router.cls.php
+++ src/router.cls.php
@@ -625,6 +625,10 @@ class Router extends Base {
 		if ( ! isset( self::$_ip ) ) {
 			self::$_ip = self::get_ip();
 		}
+
+		if ( ! self::$_ip ) {
+			return false;
+		}
 		// $uip = explode('.', $_ip);
 		// if(empty($uip) || count($uip) != 4) Return false;
 		// foreach($ip_list as $key => $ip) $ip_list[$key] = explode('.', trim($ip));
@@ -645,16 +649,16 @@ class Router extends Base {
 	 */
 	public static function get_ip() {
 		$_ip = '';
-		if ( function_exists( 'apache_request_headers' ) ) {
-			$apache_headers = apache_request_headers();
-			$_ip = ! empty( $apache_headers['True-Client-IP'] ) ? $apache_headers['True-Client-IP'] : false;
-			if ( ! $_ip ) {
-				$_ip = ! empty( $apache_headers['X-Forwarded-For'] ) ? $apache_headers['X-Forwarded-For'] : false;
-				$_ip = explode( ',', $_ip );
-				$_ip = $_ip[ 0 ];
-			}
+		// if ( function_exists( 'apache_request_headers' ) ) {
+		// 	$apache_headers = apache_request_headers();
+		// 	$_ip = ! empty( $apache_headers['True-Client-IP'] ) ? $apache_headers['True-Client-IP'] : false;
+		// 	if ( ! $_ip ) {
+		// 		$_ip = ! empty( $apache_headers['X-Forwarded-For'] ) ? $apache_headers['X-Forwarded-For'] : false;
+		// 		$_ip = explode( ',', $_ip );
+		// 		$_ip = $_ip[ 0 ];
+		// 	}
 
-		}
+		// }
 
 		if ( ! $_ip ) {
 			$_ip = ! empty( $_SERVER['REMOTE_ADDR'] ) ? $_SERVER['REMOTE_ADDR'] : false;
