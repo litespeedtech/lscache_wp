dc2ba406ad8e8efa10347f75a3590e74e55f16f1
usabe <litespeed.abe@gmail.com>
2021-11-15 14:27:44 -0500

    Add compatibility for Perfmatters plugin on its script manager

---
+++ thirdparty/perfmatters.cls.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The Third Party integration with the Perfmatters plugin.
+ *
+ * @since		4.4.5
+ */
+namespace LiteSpeed\Thirdparty;
+defined( 'WPINC' ) || exit;
+
+class Perfmatters
+{
+	public static function preload()
+	{
+		if ( ! defined( 'PERFMATTERS_VERSION' ) ) return;
+
+		if ( is_admin() ) return;
+
+		if ( has_action( 'shutdown','perfmatters_script_manager' ) !== false ) {
+			add_action( 'init', __CLASS__ . '::disable_litespeed_esi', 4 );
+		}
+	}
+
+	public static function disable_litespeed_esi()
+	{
+		define( 'LITESPEED_ESI_OFF', true );
+	}
+}
