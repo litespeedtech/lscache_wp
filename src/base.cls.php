93e6deb008695d9aaa05c41ebc31680bb1de6cdc
Hai Zheng <hzheng@litespeedtech.com>
2022-04-20 14:21:53 -0400

    20220407 master auto cdn setup (#456)
    * [FEATURE] Initial implementation of Auto CDN Setup.
    
    Auto CDN Setup is intended to simplify the process of setting up
    QUIC.cloud CDN. This is a three step procedure:
    1. Create/login to a QC account.
    2. Click the Run Setup button
    3. Eventually receiving the nameservers to use for the CDN.
    
    Everything in between is handled by the plugin and QC core.
    
    This commit contains the following:
     * A button to link to a QUIC.cloud account. This will open the
    QUIC.cloud login page and after logging in, it will redirect back to
    wordpress.
     * A button to run CDN setup. This performs everything through DNS
    detection.
     * Eventually, it will have nameservers available for the user to update
    their nameservers to.
    
    The interface could use a lot of work, but the general execution flow is
    there.
    
    * [REFACTOR] Fix a couple bugs found during testing.
    
    * [REFACTOR] Improve CDN setup reset to reset QC side
    
    * [UPDATE] Add summary for marking a CDN Setup as done.
    
    Co-authored-by: Kevin Fwu <kfwu@litespeedtech.com>

--- src/base.cls.php
+++ src/base.cls.php
@@ -260,6 +260,8 @@ class Base extends Root {
 	const O_CDN_CLOUDFLARE_ZONE = 'cdn-cloudflare_zone';
 	const O_CDN_MAPPING 		= 'cdn-mapping';
 	const O_CDN_ATTR 			= 'cdn-attr';
+	const O_QC_TOKEN 			= 'qc-token';
+	const O_QC_NAMESERVERS		= 'qc-nameservers';
 
 	const NETWORK_O_USE_PRIMARY = 'use_primary_settings';
 
@@ -310,6 +312,8 @@ class Base extends Root {
 		self::O_CDN_CLOUDFLARE_ZONE,
 		self::O_CDN_MAPPING,
 		self::O_CDN_ATTR,
+		self::O_QC_TOKEN,
+		self::O_QC_NAMESERVERS,
 	);
 
 	protected static $_default_options = array(
@@ -527,6 +531,9 @@ class Base extends Root {
 		self::O_CDN_CLOUDFLARE_ZONE => '',
 		self::O_CDN_MAPPING 		=> array(),
 		self::O_CDN_ATTR 			=> array(),
+
+		self::O_QC_TOKEN 			=> '',
+		self::O_QC_NAMESERVERS		=> '',
 
 	);
 
@@ -872,6 +879,7 @@ class Base extends Root {
 			self::O_CDN_CLOUDFLARE_KEY,
 			self::O_OBJECT_PSWD,
 			self::O_API_KEY,
+			self::O_QC_TOKEN,
 		);
 
 		return in_array( $id, $check_ids );
