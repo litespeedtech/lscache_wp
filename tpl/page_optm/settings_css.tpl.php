4a2fc09416be458dbbd23e82a0f12f0fd42c9a5e
Hai Zheng <hzheng@litespeedtech.com>
2022-06-16 11:41:36 -0400

    Fixed ucss closest server missing on GUI

--- tpl/page_optm/settings_css.tpl.php
+++ tpl/page_optm/settings_css.tpl.php
@@ -6,6 +6,7 @@ defined( 'WPINC' ) || exit;
 // exit;
 
 $css_summary = CSS::get_summary();
+$closest_server_ucss = Cloud::get_summary( 'server.' . Cloud::SVC_UCSS );
 $closest_server = Cloud::get_summary( 'server.' . Cloud::SVC_CCSS );
 
 $ccss_queue = $this->load_queue( 'ccss' );
@@ -87,8 +88,8 @@ $ucss_queue = $this->load_queue( 'ucss' );
 					<?php endif; ?>
 				<?php endif; ?>
 
-				<?php if ( $closest_server ) : ?>
-					<a href="<?php echo Utility::build_url( Router::ACTION_CLOUD, Cloud::TYPE_REDETECT_CLOUD, false, null, array( 'svc' => Cloud::SVC_CCSS ) ); ?>" data-balloon-pos="up" data-balloon-break aria-label='<?php echo sprintf( __( 'Current closest Cloud server is %s.&#10; Click to redetect.', 'litespeed-cache' ), $closest_server ); ?>' data-litespeed-cfm="<?php echo __( 'Are you sure you want to redetect the closest cloud server for this service?', 'litespeed-cache' ) ; ?>"><i class='litespeed-quic-icon'></i></a>
+				<?php if ( $closest_server_ucss ) : ?>
+					<a href="<?php echo Utility::build_url( Router::ACTION_CLOUD, Cloud::TYPE_REDETECT_CLOUD, false, null, array( 'svc' => Cloud::SVC_CCSS ) ); ?>" data-balloon-pos="up" data-balloon-break aria-label='<?php echo sprintf( __( 'Current closest Cloud server is %s.&#10; Click to redetect.', 'litespeed-cache' ), $closest_server_ucss ); ?>' data-litespeed-cfm="<?php echo __( 'Are you sure you want to redetect the closest cloud server for this service?', 'litespeed-cache' ) ; ?>"><i class='litespeed-quic-icon'></i></a>
 				<?php endif; ?>
 
 				<?php if ( ! empty( $ucss_queue ) ) : ?>
