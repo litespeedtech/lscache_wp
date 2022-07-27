174014bfa88c1b42e3cebebe116db03c7ebf760e
Eliza Witkowska <kokers@codebusters.pl>
2022-07-27 16:57:58 +0200

    Card view for profiles

--- tpl/general/profiles.tpl.php
+++ tpl/general/profiles.tpl.php
@@ -37,24 +37,272 @@ function profile( $name ) {
 }
 
 ?>
+<style>
+.litespeed-comparison-cards {
+    display: flex;
+	margin:3rem 0 2rem 0;
+	max-width:1720px;
+}
 
-<h3 class="litespeed-title">
-	<?php esc_html_e( 'LiteSpeed Cache Profiles', 'litespeed-cache' ); ?>
+.litespeed-comparison-card {
+	width: 19%;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    margin-right: -1px;
+	justify-content: space-between;
+}
+
+.litespeed-comparison-card:first-child{
+	border-top-left-radius: 5px;
+	border-bottom-left-radius: 5px;
+	overflow: hidden;
+}
+
+.litespeed-comparison-card:last-child{
+	border-top-right-radius: 5px;
+	border-bottom-right-radius: 5px;
+	overflow: hidden;
+}
+
+.litespeed-comparison-card-rec {
+    width: 23%;
+    padding-top: 1rem;
+    padding-bottom: .75rem;
+    margin-top: -1rem;
+    margin-bottom: 0.25rem;
+	border-radius:5px;
+	overflow: hidden;
+}
+
+.litespeed-comparison-card-rec .litespeed-card-header{
+	margin-top: -1rem;
+    padding-top: 1.75rem;
+    padding-bottom: 0.95rem;
+}
+
+.litespeed-comparison-card-rec .litespeed-card-content > div.litespeed-card-body {
+	font-size:14px;
+}
+
+.litespeed-comparison-card-rec .litespeed-card-action {
+    margin-bottom: 0.25rem;
+}
+
+.litespeed-comparison-card-rec h3 {
+    font-size: 20px;
+}
+
+
+.litespeed-card-content > div , .litespeed-card-action  {
+    padding: 0.85rem 1.25rem;
+}
+
+.litespeed-card-header {
+    border-bottom: 1px solid #eee;
+	background: #f9fafc;
+}
+
+.litespeed-card-content > div.litespeed-card-body {
+    align-self: stretch;
+    justify-content: flex-end;
+    font-size: 15px;
+	padding-bottom: 0.5rem;
+	padding-top: 1rem;
+}
+
+.litespeed-card-content > div.litespeed-card-footer {
+	
+    align-self: stretch;
+    justify-content: flex-end;
+    padding-bottom: 0;
+	padding-top: 0.25rem;
+	
+}
+
+.litespeed-card-action {
+    justify-content: flex-end;
+}
+
+
+.litespeed-comparison-card ul {
+  padding-left: 20px;
+  list-style: none;
+  list-style-position: outside;
+  margin: 0;
+}
+
+.litespeed-comparison-card li {
+	margin-bottom: 0.5em;
+    line-height: 1.4;
+}
+
+.litespeed-comparison-card li:last-child {
+	margin-bottom:0;
+}
+
+.litespeed-comparison-card ul li:before {
+  content: '✓';
+  margin-left: -1em;
+  margin-right: .350em;
+  color: #329c74;
+}
+
+</style>
+<h3 class="litespeed-title-short">
+	<?php esc_html_e( 'Litespeed Cache Profiles', 'litespeed-cache' ); ?>
 	<?php Doc::learn_more( 'https://docs.litespeedtech.com/lscache/lscwp/general/#profiles-tab' ); ?>
 </h3>
 
-<?php
-	foreach ( $profiles as $name ) {
-		profile( $name );
-	}
-?>
+<p><?php esc_html_e( 'Litespeed Cache Profiles are a pre-defined set of Profiles which can be used for Optimizing any website using Litespeed Cache easily.', 'litespeed-cache' ); ?></p>
 
-<h3 class="litespeed-title">
-	<?php esc_html_e( 'History', 'litespeed-cache' ); ?>
+<div class="litespeed-comparison-cards">
+	<div class="litespeed-comparison-card postbox">
+		<div class="litespeed-card-content">
+			<div class="litespeed-card-header">
+				<h3 class="litespeed-h3"><?php esc_html_e( 'Essentials', 'litespeed-cache' ); ?></h3>
+			</div>
+			<div class="litespeed-card-body">
+				<ul>
+					<li><?php esc_html_e( 'Default Cache', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Higher TTL', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Browser Cache', 'litespeed-cache' ); ?></li>
+				</ul>
+			</div>
+			<div class="litespeed-card-footer">
+				<h4><?php esc_html_e( 'For whom this profile is good?', 'litespeed-cache' ); ?></h4>
+				<p><?php esc_html_e( 'This profile has just the most basic number of features enabled, and is good for any website. This profile can be utilized without a Domain key, Good for cache-oriented development aswell.', 'litespeed-cache' ); ?></p>
+			</div>
+		</div>
+		<div class="litespeed-card-action">
+			<a href="<?php apply_url( 'essentials' ); ?>" class="button button-secondary" data-litespeed-cfm="<?php printf( 
+				esc_html__('This will replace your current settings and apply %1$s Profile. Do you want to continue?', 'litespeed-cache' ),
+				'Essentials'
+			); ?>"><?php esc_html_e( 'Apply Profile', 'litespeed-cache' ); ?></a>
+		</div>
+	</div>
+	<div class="litespeed-comparison-card postbox">
+		<div class="litespeed-card-content">
+			<div class="litespeed-card-header">
+				<h3 class="litespeed-h3"><?php esc_html_e( 'Basic', 'litespeed-cache' ); ?></h3>
+			</div>
+			<div class="litespeed-card-body">
+				<ul>
+					<li><?php esc_html_e( 'Everything in Essentials, Plus', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Guest Mode and Optimization', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Image Optimization', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Mobile Cache', 'litespeed-cache' ); ?></li>
+				</ul>
+			</div>
+			<div class="litespeed-card-footer">
+				<h4><?php esc_html_e( 'For whom this profile is good?', 'litespeed-cache' ); ?></h4>
+				<p><?php esc_html_e( 'This Profile enables the very essentials, plus some basic optimizations which will improve Google Pagespeed Score, and also not hamper user experience on website.', 'litespeed-cache' ); ?></p>
+				<p><?php esc_html_e( 'It is good for any website, with a LSCWP Domain key.', 'litespeed-cache' ); ?></p>
+			</div>
+		</div>
+		<div class="litespeed-card-action">
+		<a href="<?php apply_url( 'basic' ); ?>" class="button button-secondary" data-litespeed-cfm="<?php printf( 
+				esc_html__('This will replace your current settings and apply %1$s Profile. Do you want to continue?', 'litespeed-cache' ),
+				'Basic'
+			); ?>"><?php esc_html_e( 'Apply Profile', 'litespeed-cache' ); ?></a>
+		</div>
+	</div>
+	<div class="litespeed-comparison-card litespeed-comparison-card-rec postbox">
+		<div class="litespeed-card-content">
+			<div class="litespeed-card-header">
+				<h3 class="litespeed-h3"><?php esc_html_e( 'Advanced', 'litespeed-cache' ); ?></h3>
+			</div>
+			<div class="litespeed-card-body">
+				<ul>
+					<li><?php esc_html_e( 'Everything in Basic, Plus', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'CSS, JS and HTML Minification', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Font Display Optimization', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'JS Defer for both external and inline JS', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'DNS Prefetch for static files', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Gravatar Cache', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Remove Query Strings from Static Files', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Remove WordPress Emoji', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Remove Noscript Tags', 'litespeed-cache' ); ?></li>
+				</ul>
+			</div>
+			<div class="litespeed-card-footer">
+				<h4><?php esc_html_e( 'For whom this profile is good?', 'litespeed-cache' ); ?></h4>
+				<p><?php esc_html_e( 'This profile is the most recommended one, and is enabled with most non-conflicting options, and is good for most websites.' ); ?></p>
+				<p><?php esc_html_e( 'Domain Key must be set to use this profile. Also improves your Pagespeed Score.', 'litespeed-cache' ); ?></p>
+				
+			</div>
+		</div>
+		<div class="litespeed-card-action">
+		<a href="<?php apply_url( 'advanced' ); ?>" class="button button-primary" data-litespeed-cfm="<?php printf( 
+				esc_html__('This will replace your current settings and apply %1$s Profile. Do you want to continue?', 'litespeed-cache' ),
+				'Advanced'
+			); ?>"><?php esc_html_e( 'Apply Profile', 'litespeed-cache' ); ?></a>
+		</div>
+	</div>
+	<div class="litespeed-comparison-card postbox">
+		<div class="litespeed-card-content">
+			<div class="litespeed-card-header">
+				<h3 class="litespeed-h3"><?php esc_html_e( 'Aggressive', 'litespeed-cache' ); ?></h3>
+			</div>
+			<div class="litespeed-card-body">
+				<ul>
+					<li><?php esc_html_e( 'Everything in Advanced, Plus', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'CSS & JS Combine', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Asynchronous CSS Loading with Critical CSS', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Removed Unused CSS for Users', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Lazy Load for Iframes', 'litespeed-cache' ); ?></li>
+				</ul>
+			</div>
+			<div class="litespeed-card-footer">
+				<h4><?php esc_html_e( 'For whom this profile is good?', 'litespeed-cache' ); ?></h4>
+				<p><?php esc_html_e( 'This profile while might work out of box in some websites, but might require some excludes in the Page Optimization >> Tuning section. ' ); ?></p>
+				<p><?php esc_html_e( 'Domain Key must be set to use this profile. Also improves your Pagespeed Score.', 'litespeed-cache' ); ?></p>
+				
+			</div>
+		</div>
+		<div class="litespeed-card-action">
+			<a href="<?php apply_url( 'aggressive' ); ?>" class="button button-secondary" data-litespeed-cfm="<?php printf( 
+				esc_html__('This will replace your current settings and apply %1$s Profile. Do you want to continue?', 'litespeed-cache' ),
+				'Aggressive'
+			); ?>"><?php esc_html_e( 'Apply Profile', 'litespeed-cache' ); ?></a>
+		</div>
+	</div>
+	<div class="litespeed-comparison-card postbox">
+		<div class="litespeed-card-content">
+			<div class="litespeed-card-header">
+				<h3 class="litespeed-h3"><?php esc_html_e( 'Extreme', 'litespeed-cache' ); ?></h3>
+			</div>
+			<div class="litespeed-card-body">
+				<ul>
+					<li><?php esc_html_e( 'Everything in Aggressive, Plus', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'LazyLoad for Images', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'JS Delayed', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Inline JS added to Combine', 'litespeed-cache' ); ?></li>
+					<li><?php esc_html_e( 'Inline CSS added to Combine', 'litespeed-cache' ); ?></li>
+				</ul>
+			</div>
+			<div class="litespeed-card-footer">
+				<h4><?php esc_html_e( 'For whom this profile is good?', 'litespeed-cache' ); ?></h4>
+				<p><?php esc_html_e( 'This profile will most likely require the website owner to work with Lazy Load Exclusions specially for images such as Logo, or HTML based Slider Images on Top.' ); ?></p>
+				<p><?php esc_html_e( 'Domain Key must be set to use this profile. Also improves your Pagespeed Score.', 'litespeed-cache' ); ?></p>
+				
+			</div>
+		</div>
+		<div class="litespeed-card-action">
+		<a href="<?php apply_url( 'extreme' ); ?>" class="button button-secondary" data-litespeed-cfm="<?php printf( 
+				esc_html__('This will replace your current settings and apply %1$s Profile. Do you want to continue?', 'litespeed-cache' ),
+				'Extreme'
+			); ?>"><?php esc_html_e( 'Apply Profile', 'litespeed-cache' ); ?></a>
+		</div>
+	</div>
+</div>
+
+<h3 class="litespeed-title-short">
+	<?php esc_html_e( 'Profile Revision', 'litespeed-cache' ); ?>
 </h3>
 
 <?php if ( ! empty( $summary['profile'] ) ) : ?>
-<div class="litespeed-desc">
+<p>
 	<?php
 	printf(
 		esc_html__( 'Last applied the %1$s profile %2$s', 'litespeed-cache' ),
@@ -62,12 +310,16 @@ function profile( $name ) {
 		Utility::readable_time( $summary['profile_time'] )
 	);
 	?>
-</div>
-<?php endif; ?>
+</p>
 
 <div><a
 		href="<?php echo Utility::build_url( Router::ACTION_PROFILES, Profiles::TYPE_REVERT ); ?>"
-		class="button button-primary"
+		class="button button-secondary" data-litespeed-cfm="<?php esc_html_e( 'This will revert to settings before this profile has been applied. Any changes made after will be lost. Do you want to continue?', 'litespeed-cache' ) ; ?>"
 	>
-	<?php esc_html_e( 'Revert', 'litespeed-cache' ); ?>
+	<?php esc_html_e( 'Revert to Previous Settings', 'litespeed-cache' ); ?>
 </a></div>
+
+<?php else: ?>
+	<p class="litespeed-desc"><?php esc_html_e( 'No revisions are available', 'litespeed-cache' ); ?></p>
+<?php endif; ?>
+
