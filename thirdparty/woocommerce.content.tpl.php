04abee989331303bbad0d5e013000cf768425fad
tynanbe <contact@tynan.be>
2022-10-13 22:20:42 -0500

    Remove WooCommerce private cache

--- thirdparty/woocommerce.content.tpl.php
+++ thirdparty/woocommerce.content.tpl.php
@@ -27,18 +27,6 @@ use \LiteSpeed\Base;
 <table class="wp-list-table striped litespeed-table"><tbody>
 	<tr>
 		<th>
-			<?php $id = self::O_ESI_CACHE_CART; ?>
-			<?php echo __( 'Use ESI for Cart', 'litespeed-cache' ); ?>
-		</th>
-		<td>
-			<?php do_action( 'litespeed_build_switch' , $id ); ?>
-			<div class="litespeed-desc">
-				<?php echo sprintf( __( 'When the option is ON, cart information will be implemented as ESI blocks. NOTE: To make it work, you need to turn on %1$s under ESI settings.', 'litespeed-cache' ), '<code>' . Lang::title( Base::O_ESI ) . '</code>' ); ?>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<th>
 			<?php $id = self::O_UPDATE_INTERVAL; ?>
 			<?php echo __( 'Product Update Interval', 'litespeed-cache' ); ?>
 		</th>
@@ -62,33 +50,6 @@ use \LiteSpeed\Base;
 			<?php endforeach; ?>
 			<div class="litespeed-desc">
 				<?php echo __( 'Determines how changes in product quantity and product stock status affect product pages and their associated category pages.', 'litespeed-cache' ); ?>
-			</div>
-		</td>
-	</tr>
-
-	<tr>
-		<th>
-			<?php $id = self::O_SHOP_FRONT_TTL; ?>
-			<?php echo __( 'Use Front Page TTL for the Shop Page', 'litespeed-cache' ); ?>
-		</th>
-		<td>
-			<?php do_action( 'litespeed_build_switch', $id ); ?>
-			<div class="litespeed-desc">
-				<?php echo __( 'Checking this option will force the shop page to use the front page TTL setting.', 'litespeed-cache' ); ?>
-				<?php echo sprintf( __( 'For example, if the homepage for the site is located at %1$s, the shop page may be located at %2$s.', 'litespeed-cache' ), 'https://www.EXAMPLE.com', 'https://www.EXAMPLE.com/shop' ); ?>
-			</div>
-		</td>
-	</tr>
-
-	<tr>
-		<th>
-			<?php $id = self::O_WOO_CACHE_CART; ?>
-			<?php echo __( 'Privately Cache Cart', 'litespeed-cache' ); ?>
-		</th>
-		<td>
-			<?php do_action( 'litespeed_build_switch', $id ); ?>
-			<div class="litespeed-desc">
-				<?php echo __( 'Privately cache cart when not empty.', 'litespeed-cache' ); ?>
 			</div>
 		</td>
 	</tr>
