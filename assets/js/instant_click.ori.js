08424fbc90b48e2869a9d191c8601a882ddf7318
Timotei <bondastimotei@gmail.com>
2024-04-15 16:16:01 +0300

    Update to version 5.2.0

--- assets/js/instant_click.ori.js
+++ assets/js/instant_click.ori.js
@@ -1,236 +1,117 @@
-/*! instant.page v5.1.0 - (C) 2019-2020 Alexandre Dieulot - https://instant.page/license */
+/*! instant.page v5.2.0 - (C) 2019-2023 Alexandre Dieulot - https://instant.page/license */
+let t, e, n, o, i, a = null,
+    s = 65,
+    c = new Set;
+const r = 1111;
 
-let mouseoverTimer
-let lastTouchTimestamp
-const prefetches = new Set()
-const prefetchElement = document.createElement('link')
-const isSupported = prefetchElement.relList && prefetchElement.relList.supports && prefetchElement.relList.supports('prefetch')
-                    && window.IntersectionObserver && 'isIntersecting' in IntersectionObserverEntry.prototype
-const allowQueryString = 'instantAllowQueryString' in document.body.dataset
-const allowExternalLinks = 'instantAllowExternalLinks' in document.body.dataset
-const useWhitelist = 'instantWhitelist' in document.body.dataset
-const mousedownShortcut = 'instantMousedownShortcut' in document.body.dataset
-const DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION = 1111
+function d(t) {
+    o = performance.now();
+    const e = t.target.closest("a");
+    m(e) && p(e.href, "high")
+}
 
-let delayOnHover = 65
-let useMousedown = false
-let useMousedownOnly = false
-let useViewport = false
+function u(t) {
+    if (performance.now() - o < r) return;
+    if (!("closest" in t.target)) return;
+    const e = t.target.closest("a");
+    m(e) && (e.addEventListener("mouseout", f, {
+        passive: !0
+    }), i = setTimeout(() => {
+        p(e.href, "high"), i = void 0
+    }, s))
+}
 
-if ('instantIntensity' in document.body.dataset) {
-  const intensity = document.body.dataset.instantIntensity
+function l(t) {
+    const e = t.target.closest("a");
+    m(e) && p(e.href, "high")
+}
 
-  if (intensity.substr(0, 'mousedown'.length) == 'mousedown') {
-    useMousedown = true
-    if (intensity == 'mousedown-only') {
-      useMousedownOnly = true
-    }
-  }
-  else if (intensity.substr(0, 'viewport'.length) == 'viewport') {
-    if (!(navigator.connection && (navigator.connection.saveData || (navigator.connection.effectiveType && navigator.connection.effectiveType.includes('2g'))))) {
-      if (intensity == "viewport") {
-        /* Biggest iPhone resolution (which we want): 414 × 896 = 370944
-         * Small 7" tablet resolution (which we don’t want): 600 × 1024 = 614400
-         * Note that the viewport (which we check here) is smaller than the resolution due to the UI’s chrome */
-        if (document.documentElement.clientWidth * document.documentElement.clientHeight < 450000) {
-          useViewport = true
+function f(t) {
+    t.relatedTarget && t.target.closest("a") == t.relatedTarget.closest("a") || i && (clearTimeout(i), i = void 0)
+}
+
+function h(t) {
+    if (performance.now() - o < r) return;
+    const e = t.target.closest("a");
+    if (t.which > 1 || t.metaKey || t.ctrlKey) return;
+    if (!e) return;
+    e.addEventListener("click", function(t) {
+        1337 != t.detail && t.preventDefault()
+    }, {
+        capture: !0,
+        passive: !1,
+        once: !0
+    });
+    const n = new MouseEvent("click", {
+        view: window,
+        bubbles: !0,
+        cancelable: !1,
+        detail: 1337
+    });
+    e.dispatchEvent(n)
+}
+
+function m(o) {
+    if (o && o.href && (!n || "instant" in o.dataset)) {
+        if (o.origin != location.origin) {
+            if (!(e || "instant" in o.dataset) || !a) return
         }
-      }
-      else if (intensity == "viewport-all") {
-        useViewport = true
-      }
+        if (["http:", "https:"].includes(o.protocol) && ("http:" != o.protocol || "https:" != location.protocol) && (t || !o.search || "instant" in o.dataset) && !(o.hash && o.pathname + o.search == location.pathname + location.search || "noInstant" in o.dataset)) return !0
     }
-  }
-  else {
-    const milliseconds = parseInt(intensity)
-    if (!isNaN(milliseconds)) {
-      delayOnHover = milliseconds
-    }
-  }
 }
 
-if (isSupported) {
-  const eventListenersOptions = {
-    capture: true,
-    passive: true,
-  }
-
-  if (!useMousedownOnly) {
-    document.addEventListener('touchstart', touchstartListener, eventListenersOptions)
-  }
-
-  if (!useMousedown) {
-    document.addEventListener('mouseover', mouseoverListener, eventListenersOptions)
-  }
-  else if (!mousedownShortcut) {
-      document.addEventListener('mousedown', mousedownListener, eventListenersOptions)
-  }
-
-  if (mousedownShortcut) {
-    document.addEventListener('mousedown', mousedownShortcutListener, eventListenersOptions)
-  }
-
-  if (useViewport) {
-    let triggeringFunction
-    if (window.requestIdleCallback) {
-      triggeringFunction = (callback) => {
-        requestIdleCallback(callback, {
-          timeout: 1500,
-        })
-      }
-    }
-    else {
-      triggeringFunction = (callback) => {
-        callback()
-      }
-    }
-
-    triggeringFunction(() => {
-      const intersectionObserver = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            const linkElement = entry.target
-            intersectionObserver.unobserve(linkElement)
-            preload(linkElement.href)
-          }
-        })
-      })
-
-      document.querySelectorAll('a').forEach((linkElement) => {
-        if (isPreloadable(linkElement)) {
-          intersectionObserver.observe(linkElement)
+function p(t, e = "auto") {
+    if (c.has(t)) return;
+    const n = document.createElement("link");
+    n.rel = "prefetch", n.href = t, n.fetchPriority = e, n.as = "document", document.head.appendChild(n), c.add(t)
+}! function() {
+    if (!document.createElement("link").relList.supports("prefetch")) return;
+    const o = "instantVaryAccept" in document.body.dataset || "Shopify" in window,
+        i = navigator.userAgent.indexOf("Chrome/");
+    i > -1 && (a = parseInt(navigator.userAgent.substring(i + "Chrome/".length)));
+    if (o && a && a < 110) return;
+    const c = "instantMousedownShortcut" in document.body.dataset;
+    t = "instantAllowQueryString" in document.body.dataset, e = "instantAllowExternalLinks" in document.body.dataset, n = "instantWhitelist" in document.body.dataset;
+    const r = {
+        capture: !0,
+        passive: !0
+    };
+    let f = !1,
+        v = !1,
+        g = !1;
+    if ("instantIntensity" in document.body.dataset) {
+        const t = document.body.dataset.instantIntensity;
+        if (t.startsWith("mousedown")) f = !0, "mousedown-only" == t && (v = !0);
+        else if (t.startsWith("viewport")) {
+            const e = navigator.connection && navigator.connection.saveData,
+                n = navigator.connection && navigator.connection.effectiveType && navigator.connection.effectiveType.includes("2g");
+            e || n || ("viewport" == t ? document.documentElement.clientWidth * document.documentElement.clientHeight < 45e4 && (g = !0) : "viewport-all" == t && (g = !0))
+        } else {
+            const e = parseInt(t);
+            isNaN(e) || (s = e)
         }
-      })
-    })
-  }
-}
-
-function touchstartListener(event) {
-  /* Chrome on Android calls mouseover before touchcancel so `lastTouchTimestamp`
-   * must be assigned on touchstart to be measured on mouseover. */
-  lastTouchTimestamp = performance.now()
-
-  const linkElement = event.target.closest('a')
-
-  if (!isPreloadable(linkElement)) {
-    return
-  }
-
-  preload(linkElement.href)
-}
-
-function mouseoverListener(event) {
-  if (performance.now() - lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) {
-    return
-  }
-
-  const linkElement = event.target.closest('a')
-
-  if (!isPreloadable(linkElement)) {
-    return
-  }
-
-  linkElement.addEventListener('mouseout', mouseoutListener, {passive: true})
-
-  mouseoverTimer = setTimeout(() => {
-    preload(linkElement.href)
-    mouseoverTimer = undefined
-  }, delayOnHover)
-}
-
-function mousedownListener(event) {
-  const linkElement = event.target.closest('a')
-
-  if (!isPreloadable(linkElement)) {
-    return
-  }
-
-  preload(linkElement.href)
-}
-
-function mouseoutListener(event) {
-  if (event.relatedTarget && event.target.closest('a') == event.relatedTarget.closest('a')) {
-    return
-  }
-
-  if (mouseoverTimer) {
-    clearTimeout(mouseoverTimer)
-    mouseoverTimer = undefined
-  }
-}
-
-function mousedownShortcutListener(event) {
-  if (performance.now() - lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) {
-    return
-  }
-
-  const linkElement = event.target.closest('a')
-
-  if (event.which > 1 || event.metaKey || event.ctrlKey) {
-    return
-  }
-
-  if (!linkElement) {
-    return
-  }
-
-  linkElement.addEventListener('click', function (event) {
-    if (event.detail == 1337) {
-      return
     }
-
-    event.preventDefault()
-  }, {capture: true, passive: false, once: true})
-
-  const customEvent = new MouseEvent('click', {view: window, bubbles: true, cancelable: false, detail: 1337})
-  linkElement.dispatchEvent(customEvent)
-}
-
-function isPreloadable(linkElement) {
-  if (!linkElement || !linkElement.href) {
-    return
-  }
-
-  if (useWhitelist && !('instant' in linkElement.dataset)) {
-    return
-  }
-
-  if (!allowExternalLinks && linkElement.origin != location.origin && !('instant' in linkElement.dataset)) {
-    return
-  }
-
-  if (!['http:', 'https:'].includes(linkElement.protocol)) {
-    return
-  }
-
-  if (linkElement.protocol == 'http:' && location.protocol == 'https:') {
-    return
-  }
-
-  if (!allowQueryString && linkElement.search && !('instant' in linkElement.dataset)) {
-    return
-  }
-
-  if (linkElement.hash && linkElement.pathname + linkElement.search == location.pathname + location.search) {
-    return
-  }
-
-  if ('noInstant' in linkElement.dataset) {
-    return
-  }
-
-  return true
-}
-
-function preload(url) {
-  if (prefetches.has(url)) {
-    return
-  }
-
-  const prefetcher = document.createElement('link')
-  prefetcher.rel = 'prefetch'
-  prefetcher.href = url
-  document.head.appendChild(prefetcher)
-
-  prefetches.add(url)
-}
+    v || document.addEventListener("touchstart", d, r);
+    f ? c || document.addEventListener("mousedown", l, r) : document.addEventListener("mouseover", u, r);
+    c && document.addEventListener("mousedown", h, r);
+    if (g) {
+        let t = window.requestIdleCallback;
+        t || (t = (t => {
+            t()
+        })), t(function() {
+            const t = new IntersectionObserver(e => {
+                e.forEach(e => {
+                    if (e.isIntersecting) {
+                        const n = e.target;
+                        t.unobserve(n), p(n.href)
+                    }
+                })
+            });
+            document.querySelectorAll("a").forEach(e => {
+                m(e) && t.observe(e)
+            })
+        }, {
+            timeout: 1500
+        })
+    }
+}();
