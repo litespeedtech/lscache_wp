ec875ad7a5b3f2695fa8bf6ddd8c4355234c2ad1
Viktor Szépe <viktor@szepe.net>
2024-08-14 05:32:35 +0200

    Update headers in litespeed-cache.php

--- litespeed-cache.php
+++ litespeed-cache.php
@@ -5,6 +5,9 @@
  * Plugin URI:        https://www.litespeedtech.com/products/cache-plugins/wordpress-acceleration
  * Description:       High-performance page caching and site optimization from LiteSpeed
  * Version:           6.4-a8
+ * Requires PHP:      7.2
+ * Requires at least: 5.3
+ * Tested up to:      6.6
  * Author:            LiteSpeed Technologies
  * Author URI:        https://www.litespeedtech.com
  * License:           GPLv3
