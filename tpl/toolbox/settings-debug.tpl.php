84e4505cf6127b02068efc403bdc94edee458f51
Tim <162806658+timotei-litespeed@users.noreply.github.com>
2024-10-01 15:50:41 +0300

    Admin - Update debug log location (#753)
    * Admin - Update debug log location
    
    * Review change
    
    ---------
    
    Co-authored-by: Timotei <bondastimotei@gmail.com>

--- tpl/toolbox/settings-debug.tpl.php
+++ tpl/toolbox/settings-debug.tpl.php
@@ -48,10 +48,9 @@ $this->form_action($this->_is_network_admin ? Router::ACTION_SAVE_SETTINGS_NETWO
 			<td>
 				<?php $this->build_switch($id, array(__('OFF', 'litespeed-cache'), __('ON', 'litespeed-cache'), __('Admin IP Only', 'litespeed-cache'))); ?>
 				<div class="litespeed-desc">
-					<?php echo __('Outputs to WordPress debug log.', 'litespeed-cache'); ?>
+					<?php printf(__('Outputs to a series of files in the %s directory.', 'litespeed-cache'), '<code>wp-content/litespeed/debug</code>'); ?>
 					<?php echo __('To prevent filling up the disk, this setting should be OFF when everything is working.', 'litespeed-cache'); ?>
-					<?php echo __('The Admin IP option will only output log messages on requests from admin IPs.', 'litespeed-cache'); ?>
-					<?php echo sprintf(__('The logs will be output to %s.', 'litespeed-cache'), '<code>wp-content/debug.log</code>'); ?>
+					<?php echo __('The Admin IP option will only output log messages on requests from admin IPs listed below.', 'litespeed-cache'); ?>
 				</div>
 			</td>
 		</tr>
