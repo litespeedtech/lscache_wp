f89e04738d03bcd2a2dd8cf6024e9b3ec9a767c1
Tim <162806658+timotei-litespeed@users.noreply.github.com>
2025-02-06 11:27:47 +0200

    Update src/vary.cls.php
    Co-authored-by: Viktor Szépe <viktor@szepe.net>

--- src/vary.cls.php
+++ src/vary.cls.php
@@ -755,7 +755,7 @@ class Vary extends Root
 		$is_ssl = $this->conf(Base::O_UTIL_NO_HTTPS_VARY) ? false : is_ssl();
 
 		// Fix https://wordpress.org/support/topic/fatal-error-in-litespeed-cache-setcookie-path-issue/
-		$encoded_path = urlencode( $path ?: COOKIEPATH, COOKIE_DOMAIN );
+		$encoded_path = urlencode( $path ?: COOKIEPATH);
 
 		setcookie(self::$_vary_name, $val, $expire, $encoded_path, $is_ssl, true);
 	}
