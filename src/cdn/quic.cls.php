c026df2d82dcff3f399e2de2543270a24265f834
Hai Zheng <hai.zheng@litespeedtech.com>
2025-03-14 18:52:35 -0400

    Fixed frequent sync_conf() call when having WooCommerce installed.

--- src/cdn/quic.cls.php
+++ src/cdn/quic.cls.php
@@ -23,6 +23,7 @@ class Quic extends Base
 	const TYPE_REG = 'reg';
 
 	protected $_summary;
+	private $_force = false;
 	public function __construct()
 	{
 		$this->_summary = self::get_summary();
@@ -33,46 +34,28 @@ class Quic extends Base
 	 *
 	 * @access public
 	 */
-	public static function try_sync_config()
-	{
-		self::cls()->try_sync_conf();
-	}
-
 	public function try_sync_conf($force = false)
 	{
-		$options = $this->get_options();
+		if ($force) {
+			$this->_force = $force;
+		}
 
-		if (!$options[self::O_CDN_QUIC]) {
+		if (!$this->conf(self::O_CDN_QUIC)) {
 			if (!empty($this->_summary['conf_md5'])) {
+				self::debug('❌ No QC CDN, clear conf md5!');
 				self::save_summary(array('conf_md5' => ''));
 			}
 			return false;
 		}
 
-		// Security: Remove cf key in report
-		$secure_fields = array(self::O_CDN_CLOUDFLARE_KEY, self::O_OBJECT_PSWD);
-		foreach ($secure_fields as $v) {
-			if (!empty($options[$v])) {
-				$options[$v] = str_repeat('*', strlen($options[$v]));
-			}
-		}
-		unset($options[self::O_MEDIA_LQIP_EXC]);
-		unset($options[self::O_API_KEY]);
-		unset($options[self::_VER]);
-
-		// Remove overflow multi lines fields
-		foreach ($options as $k => $v) {
-			if (is_array($v) && count($v) > 30) {
-				$v = array_slice($v, 0, 30);
-				$options[$k] = $v;
-			}
+		// Notice: Sync conf must be after `wp_loaded` hook, to get 3rd party vary injected (e.g. `woocommerce_cart_hash`).
+		if (!did_action('wp_loaded')) {
+			add_action('wp_loaded', array($this, 'try_sync_conf'), 999);
+			self::debug('WP not loaded yet, delay sync to wp_loaded:999');
+			return;
 		}
 
-		// Rest url
-		$options['_rest'] = function_exists('rest_get_url_prefix') ? rest_get_url_prefix() : apply_filters('rest_url_prefix', 'wp-json');
-		$options['_home_url'] = home_url('/');
-
-		// Append hooks
+		$options = $this->get_options();
 		$options['_tp_cookies'] = apply_filters('litespeed_vary_cookies', array());
 
 		// Build necessary options only
@@ -80,7 +63,6 @@ class Quic extends Base
 			self::O_CACHE_DROP_QS,
 			self::O_CACHE_EXC_COOKIES,
 			self::O_CACHE_EXC_USERAGENTS,
-			self::O_CACHE_FAVICON,
 			self::O_CACHE_LOGIN_COOKIE,
 			self::O_CACHE_VARY_COOKIES,
 			self::O_CACHE_MOBILE_RULES,
@@ -90,7 +72,6 @@ class Quic extends Base
 			self::O_CACHE_TTL_BROWSER,
 			self::O_IMG_OPTM_WEBP,
 			self::O_GUEST,
-			self::O_GUEST_OPTM,
 			'_tp_cookies',
 		);
 		$consts_needed = array('WP_CONTENT_DIR', 'LSCWP_CONTENT_DIR', 'LSCWP_CONTENT_FOLDER', 'LSWCP_TAG_PREFIX');
@@ -98,8 +79,13 @@ class Quic extends Base
 		foreach ($options_needed as $v) {
 			if (isset($options[$v])) {
 				$options_for_md5[$v] = $options[$v];
+				// Remove overflow multi lines fields
+				if (is_array($options_for_md5[$v]) && count($options_for_md5[$v]) > 30) {
+					$options_for_md5[$v] = array_slice($options_for_md5[$v], 0, 30);
+				}
 			}
 		}
+
 		$server_vars = $this->server_vars();
 		foreach ($consts_needed as $v) {
 			if (isset($server_vars[$v])) {
@@ -113,7 +99,7 @@ class Quic extends Base
 		$conf_md5 = md5(\json_encode($options_for_md5));
 		if (!empty($this->_summary['conf_md5'])) {
 			if ($conf_md5 == $this->_summary['conf_md5']) {
-				if (!$force) {
+				if (!$this->_force) {
 					self::debug('Bypass sync conf to QC due to same md5', $conf_md5);
 					return;
 				}
@@ -124,7 +110,7 @@ class Quic extends Base
 		}
 
 		self::save_summary(array('conf_md5' => $conf_md5));
-		self::debug('sync conf to QC', $options_for_md5);
+		self::debug('sync conf to QC');
 
 		Cloud::post(Cloud::SVC_D_SYNC_CONF, $options_for_md5);
 	}
