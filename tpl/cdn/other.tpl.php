6deaae762880f0050f651eb664891126c656f284
Hai Zheng <hai.zheng@litespeedtech.com>
2025-05-29 13:37:09 -0400

    v7.3-a1: CDN tpl WP rules formatted.

--- tpl/cdn/other.tpl.php
+++ tpl/cdn/other.tpl.php
@@ -1,8 +1,15 @@
 <?php
+/**
+ * LiteSpeed Cache CDN Settings
+ *
+ * @package LiteSpeed
+ * @since 1.0.0
+ */
 
 namespace LiteSpeed;
 
 defined( 'WPINC' ) || exit;
+
 $home_url = home_url( '/' );
 $parsed   = parse_url( $home_url );
 $home_url = str_replace( $parsed['scheme'] . ':', '', $home_url );
@@ -16,8 +23,9 @@ if ( ! $cdn_mapping ) {
 
 $this->form_action();
 ?>
+
 <h3 class="litespeed-title-short">
-	<?php echo __( 'CDN Settings', 'litespeed-cache' ); ?>
+	<?php esc_html_e( 'CDN Settings', 'litespeed-cache' ); ?>
 	<?php Doc::learn_more( 'https://docs.litespeedtech.com/lscache/lscwp/cdn/' ); ?>
 </h3>
 
@@ -31,12 +39,20 @@ $this->form_action();
 			<td>
 				<?php $this->build_switch( $id ); ?>
 				<div class="litespeed-desc">
-					<?php printf( __( 'Turn this setting %1$s if you are using a traditional Content Delivery Network (CDN) or a subdomain for static content with QUIC.cloud CDN.', 'litespeed-cache' ), '<code>' . __( 'ON', 'litespeed-cache' ) . '</code>' ); ?>
+					<?php
+					printf(
+						esc_html__( 'Turn this setting %s if you are using a traditional Content Delivery Network (CDN) or a subdomain for static content with QUIC.cloud CDN.', 'litespeed-cache' ),
+						'<code>' . esc_html__( 'ON', 'litespeed-cache' ) . '</code>'
+					);
+					?>
 					<?php Doc::learn_more( 'https://docs.litespeedtech.com/lscache/lscwp/cdn/#use-cdn-mapping' ); ?>
-				</div>
-
-				<div class="litespeed-desc">
-					<?php printf( __( 'NOTE: QUIC.cloud CDN and Cloudflare do not use CDN Mapping. If you are are only using QUIC.cloud or Cloudflare, leave this setting %1$s.', 'litespeed-cache' ), '<code>' . __( 'OFF', 'litespeed-cache' ) . '</code>' ); ?>
+					<br>
+					<?php
+					printf(
+						esc_html__( 'NOTE: QUIC.cloud CDN and Cloudflare do not use CDN Mapping. If you are only using QUIC.cloud or Cloudflare, leave this setting %s.', 'litespeed-cache' ),
+						'<code>' . esc_html__( 'OFF', 'litespeed-cache' ) . '</code>'
+					);
+					?>
 				</div>
 			</td>
 		</tr>
@@ -54,40 +70,51 @@ $this->form_action();
 
 				<script type="text/babel">
 					ReactDOM.render(
-					<CDNMapping list={ <?php echo json_encode( $cdn_mapping ); ?> } />,
-					document.getElementById( 'litespeed_cdn_mapping_div' )
-				);
-			</script>
+						<CDNMapping list={ <?php echo json_encode( $cdn_mapping ); ?> } />,
+						document.getElementById( 'litespeed_cdn_mapping_div' )
+					);
+				</script>
 
 				<div class="litespeed-warning">
-					<?php echo __( 'NOTE', 'litespeed-cache' ); ?>:
-					<?php echo __( 'To randomize CDN hostname, define multiple hostnames for the same resources.', 'litespeed-cache' ); ?>
+					<?php esc_html_e( 'NOTE', 'litespeed-cache' ); ?>:
+					<?php esc_html_e( 'To randomize CDN hostname, define multiple hostnames for the same resources.', 'litespeed-cache' ); ?>
 				</div>
 
 				<div class="litespeed-desc">
 					<b><?php $this->title( Base::CDN_MAPPING_INC_IMG ); ?></b>:
-					<?php printf( __( 'Serve all image files through the CDN. This will affect all attachments, HTML %1$s tags, and CSS %2$s attributes.', 'litespeed-cache' ), '<code>&lt;img</code>', '<code>url()</code>' ); ?>
-
-					<br />
+					<?php
+					printf(
+						esc_html__( 'Serve all image files through the CDN. This will affect all attachments, HTML %1$s tags, and CSS %2$s attributes.', 'litespeed-cache' ),
+						'<code>&lt;img</code>',
+						'<code>url()</code>'
+					);
+					?>
+					<br>
 					<b><?php $this->title( Base::CDN_MAPPING_INC_CSS ); ?></b>:
-					<?php echo __( 'Serve all CSS files through the CDN. This will affect all enqueued WP CSS files.', 'litespeed-cache' ); ?>
-
-					<br />
+					<?php esc_html_e( 'Serve all CSS files through the CDN. This will affect all enqueued WP CSS files.', 'litespeed-cache' ); ?>
+					<br>
 					<b><?php $this->title( Base::CDN_MAPPING_INC_JS ); ?></b>:
-					<?php echo __( 'Serve all JavaScript files through the CDN. This will affect all enqueued WP JavaScript files.', 'litespeed-cache' ); ?>
-
-					<br />
+					<?php esc_html_e( 'Serve all JavaScript files through the CDN. This will affect all enqueued WP JavaScript files.', 'litespeed-cache' ); ?>
+					<br>
 					<b><?php $this->title( Base::CDN_MAPPING_FILETYPE ); ?></b>:
-					<?php echo __( 'Static file type links to be replaced by CDN links.', 'litespeed-cache' ); ?>
+					<?php esc_html_e( 'Static file type links to be replaced by CDN links.', 'litespeed-cache' ); ?>
 					<?php Doc::one_per_line(); ?>
-					<?php printf( __( 'This will affect all tags containing attributes: %1$s %2$s %3$s.', 'litespeed-cache' ), '<code>src=""</code>', '<code>data-src=""</code>', '<code>href=""</code>' ); ?>
-					<?php Doc::learn_more( 'https://docs.litespeedtech.com/lscache/lscwp/cdn/#include-file-types', __( 'Default value', 'litespeed-cache' ) ); ?>
-
-					<br />
-					<?php printf( __( 'If you turn any of the above settings OFF, please remove the related file types from the %s box.', 'litespeed-cache' ), '<b>' . __( 'Include File Types', 'litespeed-cache' ) . '</b>' ); ?>
+					<?php
+					printf(
+						esc_html__( 'This will affect all tags containing attributes: %s.', 'litespeed-cache' ),
+						'<code>src=""</code> <code>data-src=""</code> <code>href=""</code>'
+					);
+					?>
+					<?php Doc::learn_more( 'https://docs.litespeedtech.com/lscache/lscwp/cdn/#include-file-types', esc_html__( 'Default value', 'litespeed-cache' ) ); ?>
+					<br>
+					<?php
+					printf(
+						esc_html__( 'If you turn any of the above settings OFF, please remove the related file types from the %s box.', 'litespeed-cache' ),
+						'<b>' . esc_html__( 'Include File Types', 'litespeed-cache' ) . '</b>'
+					);
+					?>
 					<?php Doc::learn_more( 'https://docs.litespeedtech.com/lscache/lscwp/cdn/#include-file-types' ); ?>
 				</div>
-
 			</td>
 		</tr>
 
@@ -97,7 +124,6 @@ $this->form_action();
 				<?php $this->title( $id ); ?>
 			</th>
 			<td>
-
 				<div class="litespeed-textarea-recommended">
 					<div>
 						<?php $this->build_textarea( $id, 40 ); ?>
@@ -106,11 +132,17 @@ $this->form_action();
 						<?php $this->recommended( $id ); ?>
 					</div>
 				</div>
-
 				<div class="litespeed-desc">
-					<?php echo __( 'Specify which HTML element attributes will be replaced with CDN Mapping.', 'litespeed-cache' ); ?>
-					<?php echo __( 'Only attributes listed here will be replaced.', 'litespeed-cache' ); ?>
-					<br /><?php printf( __( 'Use the format %1$s or %2$s (element is optional).', 'litespeed-cache' ), '<code>element.attribute</code>', '<code>.attribute</code>' ); ?>
+					<?php esc_html_e( 'Specify which HTML element attributes will be replaced with CDN Mapping.', 'litespeed-cache' ); ?>
+					<?php esc_html_e( 'Only attributes listed here will be replaced.', 'litespeed-cache' ); ?>
+					<br>
+					<?php
+					printf(
+						esc_html__( 'Use the format %1$s or %2$s (element is optional).', 'litespeed-cache' ),
+						'<code>element.attribute</code>',
+						'<code>.attribute</code>'
+					);
+					?>
 					<?php Doc::one_per_line(); ?>
 				</div>
 			</td>
@@ -124,8 +156,23 @@ $this->form_action();
 			<td>
 				<?php $this->build_textarea( $id ); ?>
 				<div class="litespeed-desc">
-					<?php printf( __( 'Site URL to be served through the CDN. Beginning with %1$s. For example, %2$s.', 'litespeed-cache' ), '<code>//</code>', '<code>' . $home_url . '</code>' ); ?>
-					<br /><?php printf( __( 'Wildcard %1$s supported (match zero or more characters). For example, to match %2$s and %3$s, use %4$s.', 'litespeed-cache' ), '<code>*</code>', '<code>//www.aa.com</code>', '<code>//aa.com</code>', '<code>//*aa.com</code>' ); ?>
+					<?php
+					printf(
+						esc_html__( 'Site URL to be served through the CDN. Beginning with %1$s. For example, %2$s.', 'litespeed-cache' ),
+						'<code>//</code>',
+						'<code>' . esc_html( $home_url ) . '</code>'
+					);
+					?>
+					<br>
+					<?php
+					printf(
+						esc_html__( 'Wildcard %1$s supported (match zero or more characters). For example, to match %2$s and %3$s, use %4$s.', 'litespeed-cache' ),
+						'<code>*</code>',
+						'<code>//www.aa.com</code>',
+						'<code>//aa.com</code>',
+						'<code>//*aa.com</code>'
+					);
+					?>
 					<?php Doc::one_per_line(); ?>
 				</div>
 			</td>
@@ -145,9 +192,8 @@ $this->form_action();
 						<?php $this->recommended( $id ); ?>
 					</div>
 				</div>
-
 				<div class="litespeed-desc">
-					<?php echo __( 'Only files within these directories will be pointed to the CDN.', 'litespeed-cache' ); ?>
+					<?php esc_html_e( 'Only files within these directories will be pointed to the CDN.', 'litespeed-cache' ); ?>
 					<?php Doc::one_per_line(); ?>
 				</div>
 			</td>
@@ -161,14 +207,12 @@ $this->form_action();
 			<td>
 				<?php $this->build_textarea( $id ); ?>
 				<div class="litespeed-desc">
-					<?php echo __( 'Paths containing these strings will not be served from the CDN.', 'litespeed-cache' ); ?>
+					<?php esc_html_e( 'Paths containing these strings will not be served from the CDN.', 'litespeed-cache' ); ?>
 					<?php Doc::one_per_line(); ?>
 				</div>
 			</td>
 		</tr>
-
 	</tbody>
 </table>
 
-<?php
-$this->form_end();
+<?php $this->form_end(); ?>
