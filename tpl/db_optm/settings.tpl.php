2c303d5ed6948c54859cc42258ed2f163c9e4e51
Hai Zheng <hai.zheng@litespeedtech.com>
2025-06-05 15:31:36 -0400

    v7.3-a8: tpl/db_optm wp formatted.

--- tpl/db_optm/settings.tpl.php
+++ tpl/db_optm/settings.tpl.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * LiteSpeed Cache Database Optimization Settings
+ *
+ * Manages settings for database optimization in LiteSpeed Cache.
+ *
+ * @package LiteSpeed
+ * @since 1.0.0
+ */
+
 namespace LiteSpeed;
 
 defined( 'WPINC' ) || exit;
@@ -6,37 +15,36 @@ defined( 'WPINC' ) || exit;
 $this->form_action();
 ?>
 
-
 <h3 class="litespeed-title-short">
-	<?php echo __( 'DB Optimization Settings', 'litespeed-cache' ); ?>
+	<?php esc_html_e( 'DB Optimization Settings', 'litespeed-cache' ); ?>
 	<?php Doc::learn_more( 'https://docs.litespeedtech.com/lscache/lscwp/database/#db-optimization-settings-tab' ); ?>
 </h3>
 
 <table class="wp-list-table striped litespeed-table"><tbody>
 	<tr>
 		<th>
-			<?php $id = Base::O_DB_OPTM_REVISIONS_MAX; ?>
-			<?php $this->title( $id ); ?>
+			<?php $option_id = Base::O_DB_OPTM_REVISIONS_MAX; ?>
+			<?php $this->title( $option_id ); ?>
 		</th>
 		<td>
-			<?php $this->build_input( $id, 'litespeed-input-short' ); ?>
+			<?php $this->build_input( $option_id, 'litespeed-input-short' ); ?>
 			<div class="litespeed-desc">
-				<?php echo __( 'Specify the number of most recent revisions to keep when cleaning revisions.', 'litespeed-cache' ); ?>
-				<?php $this->_validate_ttl( $id, 1, 100, true ); ?>
+				<?php esc_html_e( 'Specify the number of most recent revisions to keep when cleaning revisions.', 'litespeed-cache' ); ?>
+				<?php $this->_validate_ttl( $option_id, 1, 100, true ); ?>
 			</div>
 		</td>
 	</tr>
 
 	<tr>
 		<th>
-			<?php $id = Base::O_DB_OPTM_REVISIONS_AGE; ?>
-			<?php $this->title( $id ); ?>
+			<?php $option_id = Base::O_DB_OPTM_REVISIONS_AGE; ?>
+			<?php $this->title( $option_id ); ?>
 		</th>
 		<td>
-			<?php $this->build_input( $id, 'litespeed-input-short' ); ?> <?php echo __( 'Day(s)', 'litespeed-cache' ); ?>
+			<?php $this->build_input( $option_id, 'litespeed-input-short' ); ?> <?php esc_html_e( 'Day(s)', 'litespeed-cache' ); ?>
 			<div class="litespeed-desc">
-				<?php echo __( 'Revisions newer than this many days will be kept when cleaning revisions.', 'litespeed-cache' ); ?>
-				<?php $this->_validate_ttl( $id, 1, 600, true ); ?>
+				<?php esc_html_e( 'Revisions newer than this many days will be kept when cleaning revisions.', 'litespeed-cache' ); ?>
+				<?php $this->_validate_ttl( $option_id, 1, 600, true ); ?>
 			</div>
 		</td>
 	</tr>
@@ -44,13 +52,5 @@ $this->form_action();
 </tbody></table>
 
 <?php
-
 $this->form_end();
-
-
-
-
-
-
-
-
+?>
