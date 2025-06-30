0cb742fe1434f643460fe95368878d5bfcad48e5
Tim LSC <timotei.litespeed@gmail.com>
2025-06-30 18:47:14 +0300

    Code formatting

--- cli/database.cls.php
+++ cli/database.cls.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * LiteSpeed CLI - database cleanup
+ *
+ * Add CLI database cleanup commands.
+ *
+ * @package LiteSpeed
+ * @since 7.3
+ */
 
 namespace LiteSpeed\CLI;
 
@@ -11,24 +19,34 @@ use WP_CLI;
 /**
  * LiteSpeed Cache Database CLI
  */
-class Database
-{
-	private $__current_blog = false;
-	private $__db;
+class Database {
+	/**
+	 * Current blog id the optimization is working on.
+	 * 
+	 * @var int|false $current_blog Current blog id.
+	 */
+	private $current_blog = false;
+	/**
+	 * Database class.
+	 * 
+	 * @var DB_Optim $db Database class.
+	 */
+	private $db;
 
-	public function __construct()
-	{
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {
 		Debug2::debug('CLI_Database init');
 
-		$this->__db = DB_Optm::cls();
+		$this->db = DB_Optm::cls();
 	}
 
 	/**
 	 * List all site domains and ids on the network.
 	 */
-	public function network_list()
-	{
-		if (!is_multisite()) {
+	public function network_list() {
+		if ( !is_multisite() ) {
 			WP_CLI::error('This is not a multisite installation!');
 
 			return;
@@ -36,8 +54,8 @@ class Database
 		$buf = WP_CLI::colorize("%CThe list of installs:%n\n");
 
 		$sites = get_sites();
-		foreach ($sites as $site) {
-			$buf .= WP_CLI::colorize('%Y' . $site->domain . $site->path . ':%n ID ' . $site->blog_id) . "\n";
+		foreach ( $sites as $site ) {
+			$buf .= WP_CLI::colorize( '%Y' . $site->domain . $site->path . ':%n ID ' . $site->blog_id ) . "\n";
 		}
 
 		WP_CLI::line($buf);
@@ -45,159 +63,163 @@ class Database
 
 	/**
 	 * Change to blog sent as param.
+	 * 
+	 * @param array $args Description.
 	 */
-	private function change_to_blog($args)
-	{
-		if (isset($args[0]) && $args[0] === 'blog') {
-			$this->__current_blog = get_current_blog_id();
-			$blogid = $args[1];
-			if (!is_numeric($blogid)) {
-				$error = WP_CLI::colorize('%RError: invalid blog id entered.%n');
-				WP_CLI::line($error);
-				$this->network_list($args);
+	private function change_to_blog( $args ) {
+		if ( isset( $args[0] ) && 'blog' === $args[0] ) {
+			$this->current_blog = get_current_blog_id();
+			$blogid             = $args[1];
+			if ( !is_numeric( $blogid ) ) {
+				$error = WP_CLI::colorize( '%RError: invalid blog id entered.%n' );
+				WP_CLI::line( $error );
+				$this->network_list( $args );
 				return;
 			}
-			$site = get_blog_details($blogid);
-			if ($site === false) {
-				$error = WP_CLI::colorize('%RError: invalid blog id entered.%n');
-				WP_CLI::line($error);
-				$this->network_list($args);
+			$site = get_blog_details( $blogid );
+			if ( false === $site ) {
+				$error = WP_CLI::colorize( '%RError: invalid blog id entered.%n' );
+				WP_CLI::line( $error );
+				$this->network_list( $args );
 				return;
 			}
-			switch_to_blog($blogid);
+			switch_to_blog( $blogid );
 		}
 	}
 
 	/**
 	 * Change to previous blog.
 	 */
-	private function change_to_default()
-	{
+	private function change_to_default() {
 		// Check if previous blog set.
-		if ($this->__current_blog) {
-			switch_to_blog($this->__current_blog);
+		if ( $this->current_blog ) {
+			switch_to_blog( $this->current_blog );
 			// Switched to previous blog.
-			$this->__current_blog = false;
+			$this->current_blog = false;
 		}
 	}
 
 	/**
-	 * Show response.
+	 * Show CLI response.
+	 * 
+	 * @param boolean $result Flag if result is scuccess or failure.
+	 * @param string  $action Action name.
 	 */
-	private function show_response($result, $action)
-	{
+	private function show_response( $result, $action ) {
 		if ($result) {
-			WP_CLI::success($result);
+			WP_CLI::success( $result );
 		} else {
-			WP_CLI::error("Error running optimization: " . $action);
+			WP_CLI::error( 'Error running optimization: ' . $action );
 		}
 	}
 
 	/**
-	 * Show response.
+	 * Clean actions function.
+	 * 
+	 * @param int   $args Action arguments.
+	 * @param array $types What data to clean.
 	 */
-	private function clean_action($args, $types)
-	{
-		$this->change_to_blog($args);
-		foreach ($types as $type) {
-			$result = $this->__db->handler_clean_db_cli($type);
-			$this->show_response($result, $type);
+	private function clean_action( $args, $types ) {
+		$this->change_to_blog( $args );
+		foreach ( $types as $type ) {
+			$result = $this->db->handler_clean_db_cli( $type );
+			$this->show_response( $result, $type );
 		}
 		$this->change_to_default();
 	}
 
 	/**
 	 * Clear posts data(revisions, orphaned, auto drafts, trashed posts).
-	 *
-	 * 	   # Start clearing posts data.
+	 *     # Start clearing posts data.
 	 *     $ wp litespeed-database clear_posts
 	 *     $ wp litespeed-database clear_posts blog 2
+	 * 
+	 * @param string $args Action arguments.
 	 */
-	public function clear_posts($args)
-	{
-		$types = [
+	public function clear_posts( $args ) {
+		$types = array(
 			'revision',
 			'orphaned_post_meta',
 			'auto_draft',
-			'trash_post'
-		];
-		$this->clean_action($args, $types);
+			'trash_post',
+		);
+		$this->clean_action( $args, $types );
 	}
 
 	/**
 	 * Clear comments(spam and trash comments).
-	 *
-	 * 	   # Start clearing comments.
+	 *     # Start clearing comments.
 	 *     $ wp litespeed-database clear_comments
 	 *     $ wp litespeed-database clear_comments blog 2
+	 * 
+	 * @param string $args Action arguments.
 	 */
-	public function clear_comments($args)
-	{
-		$types = [
+	public function clear_comments( $args ) {
+		$types = array(
 			'spam_comment',
-			'trash_comment'
-		];
-		$this->clean_action($args, $types);
+			'trash_comment',
+		);
+		$this->clean_action( $args, $types );
 	}
 
 	/**
 	 * Clear trackbacks/pingbacks.
-	 *
-	 * 	   # Start clearing trackbacks/pingbacks.
+	 *     # Start clearing trackbacks/pingbacks.
 	 *     $ wp litespeed-database clear_trackbacks
 	 *     $ wp litespeed-database clear_trackbacks blog 2
+	 * 
+	 * @param string $args Action arguments.
 	 */
-	public function clear_trackbacks($args)
-	{
-		$types = [
-			'trackback-pingback'
-		];
-		$this->clean_action($args, $types);
+	public function clear_trackbacks( $args ) {
+		$types = array(
+			'trackback-pingback',
+		);
+		$this->clean_action( $args, $types );
 	}
 
 	/**
 	 * Clear transients.
-	 *
-	 * 	   # Start clearing transients.
+	 *     # Start clearing transients.
 	 *     $ wp litespeed-database clear_transients
 	 *     $ wp litespeed-database clear_transients blog 2
+	 * 
+	 * @param string $args Action arguments.
 	 */
-	public function clear_transients($args)
-	{
-		$types = [
+	public function clear_transients( $args ) {
+		$types = array(
 			'expired_transient',
-			'all_transients'
-		];
-		$this->clean_action($args, $types);
+			'all_transients',
+		);
+		$this->clean_action( $args, $types );
 	}
 
 	/**
 	 * Optimize tables.
-	 *
-	 * 	   # Start optimizing tables.
+	 *     # Start optimizing tables.
 	 *     $ wp litespeed-database optimize_tables
 	 *     $ wp litespeed-database optimize_tables blog 2
+	 * 
+	 * @param string $args Action arguments.
 	 */
-	public function optimize_tables($args)
-	{
-		$types = [
-			'optimize_tables'
-		];
-		$this->clean_action($args, $types);
+	public function optimize_tables( $args ) {
+		$types = array(
+			'optimize_tables',
+		);
+		$this->clean_action( $args, $types );
 	}
 
 	/**
 	 * Optimize database by running all possible opreations.
-	 *
-	 * 	   # Start optimizing all.
+	 *     # Start optimizing all.
 	 *     $ wp litespeed-database optimize_all
 	 *     $ wp litespeed-database optimize_all blog 2
+	 * 
+	 * @param string $args Action arguments.
 	 */
-	public function optimize_all($args)
-	{
-		$types = [
-			'all'
-		];
-		$this->clean_action($args, $types);
+	public function optimize_all( $args ) {
+		$types = array(
+			'all',
+		);
+		$this->clean_action( $args, $types );
 	}
 }
