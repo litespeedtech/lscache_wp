8b9c9484e2e32767bb9a5d80f9a659d860e61716
Hai Zheng <hai.zheng@litespeedtech.com>
2025-12-04 17:15:53 -0500

    PHPCS err fix

--- lib/guest.cls.php
+++ lib/guest.cls.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Guest vary handler for LiteSpeed Cache.
+ *
+ * NOTE: This file is loaded directly without WordPress, so WP functions are NOT available.
+ *
+ * @package LiteSpeed
+ * @since   4.1
+ */
+
 namespace LiteSpeed\Lib;
 
 /**
@@ -15,11 +24,36 @@ class Guest {
 	const O_DEBUG_IPS          = 'debug-ips';
 	const O_UTIL_NO_HTTPS_VARY = 'util-no_https_vary';
 
+	/**
+	 * Client IP address.
+	 *
+	 * @var string
+	 */
 	private static $_ip;
-	private static $_vary_name = '_lscache_vary'; // this default vary cookie is used for logged in status check
-	private $_conf             = false;
-	private $_gm_ips           = null;
-	private $_gm_uas           = null;
+
+	/**
+	 * Vary cookie name.
+	 *
+	 * @var string
+	 */
+	private static $_vary_name = '_lscache_vary';
+
+	/**
+	 * Configuration array.
+	 *
+	 * @var array|false
+	 */
+	private $_conf = false;
+
+	/**
+	 * Guest Mode lists cache.
+	 *
+	 * @var array
+	 */
+	private $_gm_lists = [
+		'ips' => null,
+		'uas' => null,
+	];
 
 	/**
 	 * Constructor
@@ -28,7 +62,7 @@ class Guest {
 	 */
 	public function __construct() {
 		! defined( 'LSCWP_CONTENT_FOLDER' ) && define( 'LSCWP_CONTENT_FOLDER', dirname( __DIR__, 3 ) );
-		// Load config
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents -- No WP available
 		$this->_conf = file_get_contents( LSCWP_CONTENT_FOLDER . '/' . self::CONF_FILE );
 		if ( $this->_conf ) {
 			$this->_conf = json_decode( $this->_conf, true );
@@ -40,15 +74,14 @@ class Guest {
 	}
 
 	/**
-	 * Update Guest vary
+	 * Update Guest vary.
 	 *
 	 * @since  4.0
+	 * @return void
 	 */
 	public function update_guest_vary() {
 		// This process must not be cached
-		/**
-		 * @reference https://wordpress.org/support/topic/soft-404-from-google-search-on-litespeed-cache-guest-vary-php/#post-16838583
-		 */
+		// @reference https://wordpress.org/support/topic/soft-404-from-google-search-on-litespeed-cache-guest-vary-php/#post-16838583
 		header( 'X-Robots-Tag: noindex' );
 		header( 'X-LiteSpeed-Cache-Control: no-cache' );
 
@@ -73,7 +106,7 @@ class Guest {
 		$is_ssl = ! empty( $this->_conf[ self::O_UTIL_NO_HTTPS_VARY ] ) ? false : $this->is_ssl();
 		setcookie( self::$_vary_name, $vary, $expire, '/', false, $is_ssl, true );
 
-		// return json
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.json_encode_json_encode -- No WP available
 		echo json_encode( [ 'reload' => 'yes' ] );
 		exit;
 	}
@@ -82,19 +115,22 @@ class Guest {
 	 * WP's is_ssl() func
 	 *
 	 * @since 4.1
+	 * @return bool
 	 */
 	private function is_ssl() {
+		// phpcs:disable WordPress.Security.ValidatedSanitizedInput -- No WP available
 		if ( isset( $_SERVER['HTTPS'] ) ) {
 			if ( 'on' === strtolower( $_SERVER['HTTPS'] ) ) {
 				return true;
 			}
 
-			if ( '1' == $_SERVER['HTTPS'] ) {
+			if ( '1' === $_SERVER['HTTPS'] ) {
 				return true;
 			}
-		} elseif ( isset( $_SERVER['SERVER_PORT'] ) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
+		} elseif ( isset( $_SERVER['SERVER_PORT'] ) && '443' === $_SERVER['SERVER_PORT'] ) {
 			return true;
 		}
+		// phpcs:enable WordPress.Security.ValidatedSanitizedInput
 		return false;
 	}
 
@@ -103,11 +139,13 @@ class Guest {
 	 *
 	 * @since 1.1.3
 	 * @access public
+	 * @return string|false
 	 */
 	public static function has_vary() {
 		if ( empty( $_COOKIE[ self::$_vary_name ] ) ) {
 			return false;
 		}
+		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash -- No WP available
 		return $_COOKIE[ self::$_vary_name ];
 	}
 
@@ -121,13 +159,12 @@ class Guest {
 	 * @return array
 	 */
 	private function _load_gm_list( $type ) {
-		$prop = '_gm_' . $type;
-		if ( null !== $this->$prop ) {
-			return $this->$prop;
+		if ( null !== $this->_gm_lists[ $type ] ) {
+			return $this->_gm_lists[ $type ];
 		}
 
-		$this->$prop = [];
-		$filename    = 'gm_' . $type . '.txt';
+		$this->_gm_lists[ $type ] = [];
+		$filename                 = 'gm_' . $type . '.txt';
 
 		// Try cloud synced file first, then fallback to plugin data file
 		$files = [
@@ -137,21 +174,23 @@ class Guest {
 
 		foreach ( $files as $file ) {
 			if ( file_exists( $file ) ) {
+				// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents -- No WP available
 				$content = file_get_contents( $file );
 				if ( $content ) {
-					$this->$prop = array_filter( array_map( 'trim', explode( "\n", $content ) ) );
+					$this->_gm_lists[ $type ] = array_filter( array_map( 'trim', explode( "\n", $content ) ) );
 					break;
 				}
 			}
 		}
 
-		return $this->$prop;
+		return $this->_gm_lists[ $type ];
 	}
 
 	/**
 	 * Detect if is a guest visitor or not
 	 *
 	 * @since  4.0
+	 * @return bool
 	 */
 	public function always_guest() {
 		if ( empty( $_SERVER['HTTP_USER_AGENT'] ) ) {
@@ -164,6 +203,7 @@ class Guest {
 			foreach ( $guest_uas as $v ) {
 				$quoted_uas[] = preg_quote( $v, '#' );
 			}
+			// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash -- No WP available
 			$match = preg_match( '#' . implode( '|', $quoted_uas ) . '#i', $_SERVER['HTTP_USER_AGENT'] );
 			if ( $match ) {
 				return true;
@@ -284,7 +324,8 @@ class Guest {
 		}
 
 		if ( ! $_ip ) {
-			$_ip = ! empty( $_SERVER['REMOTE_ADDR'] ) ? $_SERVER['REMOTE_ADDR'] : false;
+			// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash -- No WP available
+			$_ip = ! empty( $_SERVER['REMOTE_ADDR'] ) ? $_SERVER['REMOTE_ADDR'] : '';
 		}
 		return $_ip;
 	}
